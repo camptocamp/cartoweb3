ad10998e183cae492825350430018a1631403f54
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2004-10-07 20:27:08 +0000

    added recursive detection of Layers from LayerGroups

--- coreplugins/layers/server/ServerLayers.php
+++ coreplugins/layers/server/ServerLayers.php
@@ -11,15 +11,53 @@ class ServerLayers extends ServerCoreplugin {
     private $log;
 
     private $requestedLayerNames;
-
+    private $mapInfo;
+    
     function __construct() {
         parent::__construct();
         $this->log =& LoggerManager::getLogger(__CLASS__);
     }
 
+    private function getMapInfo() {
+        if(!$this->mapInfo) $this->mapInfo =& $this->serverContext->mapInfo;
+        return $this->mapInfo;
+    }
+   
+    /**
+     * Determines activated layers by recursively browsing LayerGroups.
+     * Only keeps Layer objects.
+     */
+    private function fetchChildrenFromLayerGroup($layersList) {
+        if (!$layersList || !is_array($layersList)) return false;
+
+        $cleanList = array();
+        foreach ($layersList as $key => $layerId) {
+            $serverLayer = $this->getMapInfo()->getLayerById($layerId);
+            if (!$serverLayer) continue;
+
+            // removes non Layer objects
+            if ($serverLayer instanceof Layer) $cleanList[] = $layerId;
+
+            // no use to browse more if object is not a LayerGroup
+            if (!$serverLayer instanceof LayerGroup) continue;
+            
+            // recursively gets sublayers from current layer children
+            $newList = $this->fetchChildrenFromLayerGroup($serverLayer->children);
+            if ($newList) {
+                $cleanList = array_merge($cleanList, $newList);
+                $cleanList = array_unique($cleanList);
+            }
+        }
+        return $cleanList;
+    }
+
     function getRequestedLayerNames() {
-        if (!$this->requestedLayerNames)
-            return array();
+        $this->requestedLayerNames =& 
+            $this->fetchChildrenFromLayerGroup($this->requestedLayerNames);
+
+        if(!$this->requestedLayerNames) return array();
+
+        $this->requestedLayerNames = array_unique($this->requestedLayerNames);
         return $this->requestedLayerNames;
     }
 
@@ -45,9 +83,8 @@ class ServerLayers extends ServerCoreplugin {
 
         foreach ($this->getRequestedLayerNames() as $requLayerId) {
             $this->log->debug("testing id $requLayerId");
- 
-            $mapInfo = $this->serverContext->mapInfo;
-            $serverLayer = $mapInfo->getLayerById($requLayerId);
+            
+            $serverLayer = $this->getMapInfo()->getLayerById($requLayerId);
 
             if (!$serverLayer) {
                 $this->log->warn("can't find serverLayer $requLayerId");
