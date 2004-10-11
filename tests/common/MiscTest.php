1313e66c6854ce7326c08213a3bd810c5f0c351a
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2004-10-11 15:04:25 +0000

    uses sh script for tab check.

--- tests/common/MiscTest.php
+++ tests/common/MiscTest.php
@@ -17,13 +17,21 @@ require_once 'PHPUnit2/Framework/TestCase.php';
  */
 class common_MiscTest extends PHPUnit2_Framework_TestCase {
 
+    /* uncomment if you have python > 2.3
     public function testTabs() {
 
         $ret = shell_exec('python -V 2>&1');
         $this->assertContains("Python", $ret, 'You need a python interpreter in you path');
         
-        $ret = shell_exec('python ../scripts/checktabs.py');
+        $ret = shell_exec('python ../scripts/checktabs.py 2>&1');
         $this->assertTrue(strlen($ret) == 0, "Some files contain tabs:\n$ret");        
+    }
+    */
+
+    public function testTabs() {
+
+        $ret = shell_exec('cd ../scripts/; ./checktabs.sh 2>&1');
+        $this->assertTrue(strlen($ret) == 0, "Some files contain tabs:\n$ret");
     }
 
 }
