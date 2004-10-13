9bdb1e35b30c8407d46755dfd4ddc020e6c8fd51
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2004-10-13 14:46:24 +0000

    added quotes to prevent shell expansion

--- scripts/checktabs.sh
+++ scripts/checktabs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find .. -type f -a \( -name *dist -o  -name *php -o -name *tpl -o -name *wsdl \) |xargs grep -l '	'|grep -v include/pear|grep -v '/include/'
+find .. -type f -a \( -name '*dist' -o  -name '*php' -o -name '*tpl' -o -name '*wsdl' \) |xargs grep -l '	'|grep -v include/pear|grep -v '/include/'
