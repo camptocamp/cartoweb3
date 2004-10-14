2a0b5b6a4fb9f909370c5b8de101cbd1a82322d6
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2004-10-14 16:50:36 +0000

    JS code transfered from templates/layers.js

---
+++ coreplugins/layers/htdocs/js/layers.js
@@ -0,0 +1,139 @@
+function writeOpenNodes(shortcut) {
+  if (shortcut) {
+    document.carto_form.elements.openNodes.value = openNodes;
+    return;
+  }
+  
+  var nodesList = new Array();
+  for (var i = 0; i < openNodes.length; i++) {
+    if(!isNaN(openNodes[i])) nodesList.push(openNodes[i]);
+  }
+  document.carto_form.elements.openNodes.value = nodesList;
+}
+
+function isInOpenNodes(id) {
+  for (var i = 0; i < openNodes.length; i++) {
+    if (openNodes[i] == id) return i + 1;
+  }
+  return false;
+}
+
+function updateOpenNodes(id, open) {
+  var isModified = false;
+  if (open) { 
+    if (!isInOpenNodes(id)) {
+      // adds node to list
+      openNodes.push(id);
+      isModified = true;
+    }
+  } else {
+    var i = isInOpenNodes(id);
+    if (i > 0) {
+      // removes node from list
+      delete(openNodes[i - 1]);
+      isModified = true;
+    }
+  }
+  if(isModified) writeOpenNodes();
+}
+
+function shift(id) {
+  var obj = document.getElementById(id);
+  var key = document.getElementById('x' + id);
+  var iid = id.substr(2);
+  
+  if (key.innerHTML == '-') { 
+    key.innerHTML = '+';
+    obj.style.display = 'none';
+    updateOpenNodes(iid);
+  }
+  else {
+    key.innerHTML = '-';
+    obj.style.display = 'block';
+    updateOpenNodes(iid,true);
+  }
+}
+
+ function expandAll(id) {
+  var mydiv = document.getElementById(id);
+  var divs = mydiv.getElementsByTagName('div');
+  var key;
+  
+  openNodes = new Array();
+  
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].style.display = 'block';
+    var nid = divs[i].id;
+    openNodes.push(nid.substr(2));
+    key = document.getElementById('x' + nid);
+    if (key) key.innerHTML = '-';
+  }
+
+  writeOpenNodes(true);
+}
+
+function closeAll(id) {
+  var mydiv = document.getElementById(id);
+  var divs = mydiv.getElementsByTagName('div');
+  var key;
+  
+  for (var i = 0; i < divs.length; i++) {    
+    key = document.getElementById('x' + divs[i].id);
+    if (key) key.innerHTML = '+';
+      
+    if (divs[i].getAttribute('id')) {
+        divs[i].style.display = 'none';    
+    }
+  }
+
+  openNodes = new Array();
+  writeOpenNodes(true);
+}
+
+function checkChildren(id,val) {
+  var mydiv = document.getElementById(id);
+  if (!mydiv) return;
+  
+  var divs = mydiv.getElementsByTagName('input');
+  if (val != false) val = true;
+
+  for (var i = 0; i < divs.length; i++) {
+    if (divs[i].name.substring(0, 6) == 'layers')
+      divs[i].checked = val;
+  }
+}
+
+function isChildrenChecked(id) {
+  var dparent = document.getElementById(id);
+  var celts = dparent.getElementsByTagName('input');
+  for (var i = 0; i < celts.length; i++) {
+    if (!celts[i].checked) return false;
+  }
+  return true;
+}
+
+function updateChecked(id,skipChildren) {
+  var obj = document.getElementById('in' + id);
+  if (!obj) return;
+  var val = obj.checked;
+  
+  if (!skipChildren) checkChildren('id' + id, val);
+  
+  var pid = obj.parentNode.getAttribute('id');
+  var iid = pid.substr(2);
+  var iparent = document.getElementById('in' + iid);
+ 
+  if (!iparent) return;
+
+  // if node has been unchecked, makes sure parents are unchecked too
+  if (val == false) {
+    iparent.checked = false;
+    updateChecked(iid, true);
+  }
+  // if all siblings are checked, makes sure parents are checked too
+  else if (isChildrenChecked(pid)) {
+    iparent.checked = true;
+    updateChecked(iid, true);
+  }
+}
+
