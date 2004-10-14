da92c5e2848d8007adee4b002dc6ffc9d7c4da1f
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2004-10-14 08:46:19 +0000

    substituted $_SERVER to $_ENV in CW3_PROJECT detection

--- client/ClientProjectHandler.php
+++ client/ClientProjectHandler.php
@@ -20,11 +20,11 @@ class ClientProjectHandler extends ProjectHandler {
     const PROJECT_ENV_VAR = 'CW3_PROJECT';
 
     function getProjectName () {
-        if (array_key_exists(self::PROJECT_ENV_VAR, $_ENV))
-            return $_ENV[self::PROJECT_ENV_VAR];
+        if (array_key_exists(self::PROJECT_ENV_VAR, $_SERVER))
+            return $_SERVER[self::PROJECT_ENV_VAR];
                 
-        if (array_key_exists('REDIRECT_' . self::PROJECT_ENV_VAR, $_ENV))
-            return $_ENV['REDIRECT_' . self::PROJECT_ENV_VAR];
+        if (array_key_exists('REDIRECT_' . self::PROJECT_ENV_VAR, $_SERVER))
+            return $_SERVER['REDIRECT_' . self::PROJECT_ENV_VAR];
         
         return NULL;
     }
