2510b37a2397d7dd5aeac264833c0cba4e04e40d
yves <yves@4943411f-c6a7-0410-800e-c336a95043a2>
2004-10-15 12:02:53 +0000

    Only one .mo

--- client/FormRenderer.php
+++ client/FormRenderer.php
@@ -155,9 +155,6 @@ class FormRenderer {
 
         // TODO: plugins should be able to change the flow
 
-        // Change gettext domain
-        I18n::textdomain(I18n::DEFAULT_PROJECT_DOMAIN);
-
         $smarty->display('cartoclient.tpl');
     }
 
