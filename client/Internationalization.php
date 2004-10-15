c541dbd4fc46da3d18db0d27fd47145859001446
yves <yves@4943411f-c6a7-0410-800e-c336a95043a2>
2004-10-15 12:01:47 +0000

    Language detection

--- client/Internationalization.php
+++ client/Internationalization.php
@@ -12,7 +12,6 @@
  * @package Client
  */
 class I18n {
-    
     const DEFAULT_PROJECT_DOMAIN = 'default';
     
     static private $i18n;
@@ -21,21 +20,116 @@ class I18n {
      * Initializes locales
      */
     static function init($config) {
-    
         self::$i18n = new $config->I18nClass;
+       
+        self::setLocale($config->defaultLang);
         
-        self::setLocale();
-        
-        self::$i18n->bindtextdomain(I18n::DEFAULT_PROJECT_DOMAIN, CARTOCLIENT_HOME . 'locale/');
         self::$i18n->bindtextdomain($config->mapId, CARTOCLIENT_HOME . 'locale/');
+        
+        self::$i18n->textdomain($config->mapId);
     }
+    
+    /**
+     * Returns available locales
+     */
+    static function getLocales() {
+    
+        // Looks in directory locale
+        $dir = CARTOCLIENT_HOME . 'locale/';
+        $d = dir($dir);
+        $locales = array();
+        while (false !== ($entry = $d->read())) {
+            if ($entry == '.' || $entry == '..' || strlen($entry) != 2) {
+                continue;
+            }
+            $locales[] = $entry;
+        }
+        return $locales;    
+    }
+    
+    // Detection of language in ACCEPT_LANGUAGE (mode 1) and USER_AGENT
+    // variables (mode 2). This function is used in set_language code bellow.
+    // This code is based on phpLang (http://www.phpheaven.net/projects/phpLang/).
+    static function detect_lang($lang_content, $mode)
+    {
+        $languages = array (
+            'en([-_][[:alpha:]]{2})?|english'   => array('en', 'english'),
+            'fr([-_][[:alpha:]]{2})?|french'    => array('fr', 'french'),
+            'de([-_][[:alpha:]]{2})?|german'    => array('de', 'german'),
+        );
 
+        $ext_lang = '';
+        reset($languages);
+        while ($ext_lang == '' && list($key, $name) = each($languages)) {
+            if ( ($mode == 1 &&  eregi('^'.($key).'$', trim($lang_content))) ||
+                 ($mode == 2 &&  eregi('(\(|\[|;[[:space:]])'.$key.'(;|\]|\))',
+                  $lang_content)) ) {
+                $ext_lang = $name[0];
+            }
+        }
+        return $ext_lang;
+    }
+    
     /**
      * Sets the locale depending on URL, browser or config
      */
-    static function setLocale() {
-        // TODO
-        setlocale(LC_MESSAGES, 'fr_CH');
+    static function setLocale($defaultLang) {
+        $log =& LoggerManager::getLogger(__METHOD__); 
+         
+        $locales = self::getLocales();
+        
+        // Set language code based on phpLang (www.phpheaven.net)
+        // look in LANG->cookies->$HTTP_ACCEPT_LANGUAGE->$HTTP_USER_AGENT
+        // look if LANG has been passed (by url and mod_rewrite)
+        // will work with following mod_rewrite rule:
+        // RewriteRule   ^(.*)/(fr|de|it|en)/(.*)          $1/$3 [E=LANG:$2]
+        if (isset($_SERVER['LANG']) && in_array($_SERVER['LANG'], $locales)) {
+            define('LANG', $_SERVER['LANG']);
+            $log->debug('LANG: $_SERVER[LANG] = ' . LANG);
+        }
+        // if not, looks if the language has been previously set in a cookie
+        elseif (isset($_COOKIE['LangCookie'])
+                    && in_array($_COOKIE['LangCookie'], $locales)) {
+            define('LANG', $_COOKIE['LangCookie']);
+            $log->debug('LANG: $_COOKIE[langCookie] = ' . LANG);
+        }
+        // if not in cookies, looks if valid language is set in $HTTP_ACCEPT_LANGUAGE
+        elseif (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+            $accepted_lang = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+            while (!defined('LANG') && list($key, $name) = each($accepted_lang)) {
+                $code = explode(';', $name);
+                $lang_ext = self::detect_lang($code[0], 1); //1 for ACCEPT_LANGUAGE
+                if ($lang_ext != '' && in_array($lang_ext, $locales)) {
+                    define('LANG', $lang_ext);
+                    $log->debug('LANG: $_SERVER[HTTP_ACCEPT_LANGUAGE] = ' . LANG);
+                }
+            }
+        }
+        
+        // if language not yet set, looks for language of browser in USER_AGENT cannot
+        // be elseif because ACCEPT_LANGUAGE can exist, but without valid lang.
+        if (!defined('LANG') && isset($_SERVER['HTTP_USER_AGENT'])) {
+            $lang_browser = self::detect_lang($_SERVER['HTTP_USER_AGENT'], 2); // 2:USER_AGENT
+            if ($lang_browser != '' && in_array($lang_browser, $locales)) {
+                define('LANG', $lang_browser);
+                $log->debug('LANG: $_SERVER[HTTP_USER_AGENT] = ' . LANG);
+            }
+        }
+        unset ($accepted_lang, $key, $name, $code, $lang_ext, $lang_browser);
+
+        // if language not yet set, set to default language)
+        if (!defined('LANG')) {
+            define('LANG', $defaultLang);
+            $log->debug('LANG: default = ' . LANG);
+        }
+
+        setlocale(LC_MESSAGES, LANG);
+        header('Content-Language: ' . LANG);
+        
+        // if cookie doesn't exist, set a cookie expiring in one year for current language
+        if (!isset($_COOKIE['LangCookie']) || $_COOKIE['LangCookie'] != LANG) {
+            setcookie('LangCookie', LANG, time() + 31536000);
+        }       
     }
     
     static function bindtextdomain($domain, $path) {
@@ -116,10 +210,14 @@ class I18nGettext implements I18nInterface {
 
     function bindtextdomain($domain, $path) {
         bindtextdomain($domain, $path);
+        $log =& LoggerManager::getLogger(__METHOD__); 
+        $log->debug('LANG: binddomain ' . $domain . ' ' . $path);
     }
     
     function textdomain($domain) {
         textdomain($domain);
+        $log =& LoggerManager::getLogger(__METHOD__); 
+        $log->debug('LANG: textdomain ' . $domain);
     }
     
     static function gettext($text) {
