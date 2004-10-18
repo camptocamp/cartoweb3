572e12eb3398741d36b7e303ed370f5d460863da
yves <yves@4943411f-c6a7-0410-800e-c336a95043a2>
2004-10-18 13:43:51 +0000

    New implementation (getBbox/getScale)

--- coreplugins/location/server/ServerLocation.php
+++ coreplugins/location/server/ServerLocation.php
@@ -8,7 +8,7 @@ require_once(CARTOCOMMON_HOME . 'common/basic_types.php');
 /**
  * @package CorePlugins
  */
-abstract class BboxCalculator {
+abstract class LocationCalculator {
     private $log;
     protected $requ;
 
@@ -18,12 +18,13 @@ abstract class BboxCalculator {
     }
 
     abstract function getBbox();
+    abstract function getScale();
 }
 
 /**
  * @package CorePlugins
  */
-class NoopBboxCalculator extends BboxCalculator {
+class NoopLocationCalculator extends LocationCalculator {
     private $log;
 
     function __construct($requ) {
@@ -34,79 +35,25 @@ class NoopBboxCalculator extends BboxCalculator {
     function getBbox() {
         return $this->requ->bbox;
     }
-}
-
-/**
- * @package CorePlugins
- */
-abstract class RelativeBboxCalculator extends BboxCalculator {
-    private $log;
-
-    protected $requ;
-    protected $mapExtent;
-
-    function __construct($requ, $mapExtent) {
-        $this->log =& LoggerManager::getLogger(__CLASS__);
-        $this->requ = $requ;
-        $this->mapExtent = $mapExtent;
-    }
     
-    abstract function getZoomFactor();
-    abstract function getNewCenter();
-
-    function pix2geo($pixPos, $pixMax, $geoMin, $geoMax, $isYOrig=false) {
-
-        $widthGeo = $geoMax - $geoMin;
-        $pix2geoFactor = $widthGeo / (double)$pixMax;
-
-        if ($isYOrig)
-            $pixPos = $pixMax - $pixPos;
-
-        return $geoMin + ($pixPos * $pix2geoFactor);
-
-    }
-
-    function getBbox() {
-        
-        $geoHalfX = ($this->mapExtent->maxx - $this->mapExtent->minx) / 2.0;
-        $geoHalfY = ($this->mapExtent->maxy - $this->mapExtent->miny) / 2.0;
-
-        $newCenter = $this->getNewCenter();
-        $this->log->debug("new center");
-        $this->log->debug($newCenter);
-        $newGeoX = $newCenter->x;
-        $newGeoY = $newCenter->y;
-
-        $zoomFactor = $this->getZoomFactor();
-
-        if ($zoomFactor > 1)
-            $zoomFactor = (1.0 / $zoomFactor);
-        else if ($zoomFactor < 0)
-            $zoomFactor = abs($zoomFactor);
-
-        $geoHalfX *= $zoomFactor;
-        $geoHalfY *= $zoomFactor;
-
-        $newBbox = new Bbox();
-        $newBbox->setFromBbox($newGeoX - $geoHalfX, $newGeoY - $geoHalfY, 
-                              $newGeoX + $geoHalfX, $newGeoY + $geoHalfY);
-        return $newBbox;
+    function getScale() {
+        return NULL;
     }
 }
 
 /**
  * @package CorePlugins
  */
-class PanBboxCalculator extends RelativeBboxCalculator {
+class PanLocationCalculator extends LocationCalculator {
     private $log;
+    private $oldBbox;
 
-    function __construct($requ, $mapExtent) {
-        parent::__construct($requ, $mapExtent);
+    function __construct($requ, $oldBbox) {
         $this->log =& LoggerManager::getLogger(__CLASS__);
+        parent::__construct($requ);
+        $this->oldBbox = $oldBbox;
     }
-    function getZoomFactor() {
-        return 1.0;
-    }
+
     private function panDirectionToFactor($panDirection) {
         switch ($panDirection) {
         case PanDirection::VERTICAL_PAN_NORTH:
@@ -122,76 +69,80 @@ class PanBboxCalculator extends RelativeBboxCalculator {
             throw new CartoserverException("unknown pan direction $panDirection");
         }
     }
-    function getNewCenter() {
+    
+    function getBbox() {
 
         // TODO: read from config
         $panRatio = 0.75;
 
-        $center = $this->mapExtent->getCenter();
-
-        $xOffset = $this->mapExtent->getWidth() * $panRatio * 
+        $xOffset = $this->oldBbox->getWidth() * $panRatio * 
             $this->panDirectionToFactor($this->requ->panDirection->horizontalPan);
-        $yOffset = $this->mapExtent->getHeight() * $panRatio *
+        $yOffset = $this->oldBbox->getHeight() * $panRatio *
             $this->panDirectionToFactor($this->requ->panDirection->verticalPan);
-        return new Point($center->x + $xOffset,
-                         $center->y + $yOffset);
+        $bbox = new Bbox();
+        $bbox->setFromBbox($this->oldBbox->minx + $xOffset, 
+                           $this->oldBbox->miny + $yOffset,
+                           $this->oldBbox->maxx + $xOffset,
+                           $this->oldBbox->maxy + $yOffset);
+        return $bbox;
+    }
+
+    function getScale() {
+        return NULL;
     }
 }
 
 /**
  * @package CorePlugins
  */
-class ZoomPointBboxCalculator extends RelativeBboxCalculator {
+class ZoomPointLocationCalculator extends LocationCalculator {
     private $log;
+    private $oldBbox;
+    private $oldScale;
 
-    function __construct($requ, $mapExtent) {
-        parent::__construct($requ, $mapExtent);
+    function __construct($requ, $oldBbox, $oldScale) {
         $this->log =& LoggerManager::getLogger(__CLASS__);
+        parent::__construct($requ);
+        $this->oldBbox = $oldBbox;
+        $this->oldScale = $oldScale;
     }
 
-    function getZoomFactor() {
+    function getBbox() {
+        $xHalf = $this->oldBbox->getWidth() / 2;
+        $yHalf = $this->oldBbox->getHeight() / 2;
+        
+        $bbox = new Bbox();
+        $bbox->setFromBbox($this->requ->point->x - $xHalf,
+                           $this->requ->point->y - $yHalf,
+                           $this->requ->point->x + $xHalf,
+                           $this->requ->point->y + $yHalf);
+        return $bbox;
+    }
+
+    function getScale() {
+
         switch ($this->requ->zoomType) {
         case ZoomPointLocationRequest::ZOOM_DIRECTION_IN:
-            return 2.0;//TODO: read config
+            return $this->oldScale / 2.0;//TODO: read config
         case ZoomPointLocationRequest::ZOOM_DIRECTION_NONE:
-            return 1.0;
+            return NULL;
         case ZoomPointLocationRequest::ZOOM_DIRECTION_OUT:
-            return -2.0;//TODO: read config
+            return $this->oldScale * 2.0;//TODO: read config
         case ZoomPointLocationRequest::ZOOM_FACTOR:
-            return $this->requ->zoomFactor;
+            $zoom = $this->requ->zoomFactor;
+            if ($zoom > 1)
+                $zoom = (1.0 / $zoom);
+            else if ($zoom < 0)
+                $zoom = abs($zoom);
+            return $this->oldScale * $zoom;
         case ZoomPointLocationRequest::ZOOM_SCALE:
-            return 1;
+            return $this->requ->scale;
+        default:
+            throw new CartoserverException("unknown zoom type " .
+                                           $this->requ->zoomType);
         }
-        throw new CartoclientException("unknown zoom type " .
-                                       $this->requ->zoomType);
-    }
-
-    function getNewCenter() {
-        return $this->requ->point;
     }
 }
-
-/*
-FIXME: this is not used.
-
-class ZoomRectBboxCalculator extends RelativeBboxCalculator {
-    private $log;
-
-    function __construct($requ, $imageDimension, $mapExtent) {
-        parent::__construct($requ, $imageDimension, $mapExtent);
-        $this->log =& LoggerManager::getLogger(__CLASS__);
-    }
-
-    function getZoomFactor() {
-        // TODO
-        return 1.0;
-    }
-
-    function getNewImageCenter() {
-        // TODO
-    }
-}
-*/
 
 /**
  * @package CorePlugins
@@ -215,47 +166,46 @@ class ServerLocation extends ServerCorePlugin {
 
         switch($requ->locationType) {
         case LocationRequest::LOC_REQ_BBOX:
-            $oldBbox = $requ->bboxLocationRequest->bbox;
-            $bboxCalculator = new NoopBboxCalculator($requ->bboxLocationRequest);
+            $locCalculator = new NoopLocationCalculator(
+                                        $requ->bboxLocationRequest);
             break;
         case LocationRequest::LOC_REQ_PAN:
             $oldBbox = $requ->panLocationRequest->bbox;
-            $bboxCalculator = new PanBboxCalculator(
-                $requ->panLocationRequest,
-                $oldBbox);
+            $locCalculator = new PanLocationCalculator(
+                                        $requ->panLocationRequest, $oldBbox);
             break;
         case LocationRequest::LOC_REQ_ZOOM_POINT:
             $oldBbox = $requ->zoomPointLocationRequest->bbox;
-            $bboxCalculator = new ZoomPointBboxCalculator(
-                $requ->zoomPointLocationRequest,
-                $oldBbox);
+            $msMapObj->setExtent($oldBbox->minx, $oldBbox->miny, 
+                                 $oldBbox->maxx, $oldBbox->maxy);
+            $oldScale = $msMapObj->scale;
+            
+            $locCalculator = new ZoomPointLocationCalculator(
+                                        $requ->zoomPointLocationRequest,
+                                        $oldBbox, $oldScale);
             break;
         default:
             throw new CartoserverException('unknown location request type: ' . $requ->locationType);
         }
 
-        $bbox = $bboxCalculator->getBbox();
+        // Centering
+        $bbox = $locCalculator->getBbox();
         if (!$bbox)
             throw new CartoserverException("Location plugin could not calculate bbox");
-
-        $this->log->debug("old bbox:");
-        $this->log->debug($oldBbox);
-        $this->log->debug("new bbox:");
-        $this->log->debug($bbox);
 
         $msMapObj->setExtent($bbox->minx, $bbox->miny, 
                              $bbox->maxx, $bbox->maxy);
 
-        // FIXME: Should be replaced by a more general scales management 
-        if ($requ->locationType == LocationRequest::LOC_REQ_ZOOM_POINT) {
-            if ($requ->zoomPointLocationRequest->zoomType == ZoomPointLocationRequest::ZOOM_SCALE) {
-                $center = ms_newPointObj();
-                $center->setXY($msMapObj->width/2, $msMapObj->height/2); 
-                $msMapObj->zoomscale($requ->zoomPointLocationRequest->scale,
-                            $center, $msMapObj->width, $msMapObj->height, $msMapObj->extent);
-            }
-        }
+        // Scaling
+        $scale = $locCalculator->getScale();
         
+        if ($scale) {
+            $center = ms_newPointObj();
+            $center->setXY($msMapObj->width/2, $msMapObj->height/2); 
+            $msMapObj->zoomscale($scale, $center,
+                        $msMapObj->width, $msMapObj->height, $msMapObj->extent);
+        }
+
         $location->bbox = new Bbox();
         $location->bbox->setFromMsExtent($msMapObj->extent);
         $location->scale = $msMapObj->scale;
