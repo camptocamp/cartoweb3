51413af3b94b79cf7333863136045de0c39151a5
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2004-10-27 14:51:23 +0000

    * added icon, minScale, maxScale support * some formating

--- common/MapInfo.php
+++ common/MapInfo.php
@@ -33,11 +33,9 @@ class LayerBase extends Serializable {
         $this->id    = self::unserializeValue($struct, 'id'); 
         $this->label = self::unserializeValue($struct, 'label');
         $this->link  = self::unserializeValue($struct, 'link');
-
-        // Not implemented
-        //$this->minScale = $struct->minscale;
-        //$this->maxScale = $struct->maxscale;
-        //$this->icon     = $struct->icon;
+        $this->minScale = self::unserializeValue($struct, 'minScale', 'int');
+        $this->maxScale = self::unserializeValue($struct, 'maxScale', 'int');
+        $this->icon  = self::unserializeValue($struct, 'icon');
     }
 }
 
@@ -79,8 +77,9 @@ class Layer extends LayerContainer {
 
     function unserialize($struct) {
         parent::unserialize($struct);
-        $this->msLayer = self::unserializeValue($struct, 'msLayer'); 
-        $this->idAttributeString = self::unserializeValue($struct, 'idAttributeString'); 
+        $this->msLayer           = self::unserializeValue($struct, 'msLayer'); 
+        $this->idAttributeString = self::unserializeValue($struct, 
+                                       'idAttributeString'); 
     }
 }
 
@@ -141,8 +140,10 @@ class InitialMapState extends Serializable {
 
     function unserialize($struct) {
         $this->id       = self::unserializeValue($struct, 'id');
-        $this->location = self::unserializeObject($struct, 'location', 'InitialLocation');
-        $this->layers   = self::unserializeObjectMap($struct, 'layers', 'LayerState'); 
+        $this->location = self::unserializeObject($struct, 'location', 
+                              'InitialLocation');
+        $this->layers   = self::unserializeObjectMap($struct, 'layers', 
+                              'LayerState'); 
     }
 }
 
@@ -153,6 +154,7 @@ class MapInfo extends Serializable {
     public $timeStamp;
     public $mapLabel;
     public $loadPlugins;
+    public $autoClassLegend;
     public $layers;
     public $initialMapStates;
     public $extent;
@@ -164,17 +166,25 @@ class MapInfo extends Serializable {
         $this->mapLabel         = self::unserializeValue($struct, 'mapLabel');
   
         $this->loadPlugins      = self::unserializeArray($struct, 'loadPlugins');
+        $this->autoClassLegend  = self::unserializeValue($struct, 
+                                      'autoClassLegend', 'boolean');
   
         // Layers class names are specicified in className attribute
         $this->layers           = self::unserializeObjectMap($struct, 'layers');
-        $this->initialMapStates = self::unserializeObjectMap($struct, 'initialMapStates', 'InitialMapState');
-        $this->extent           = self::unserializeObject($struct, 'extent', 'Bbox');
-        $this->location         = self::unserializeObject($struct, 'location', 'Location');
-        $this->keymapGeoDimension = self::unserializeObject($struct, 'keymapGeoDimension', 'GeoDimension');
+        $this->initialMapStates = self::unserializeObjectMap($struct, 
+                                      'initialMapStates', 
+                                      'InitialMapState');
+        $this->extent           = self::unserializeObject($struct, 'extent', 
+                                      'Bbox');
+        $this->location         = self::unserializeObject($struct, 'location',
+                                      'Location');
+        $this->keymapGeoDimension = self::unserializeObject($struct, 
+                                      'keymapGeoDimension', 'GeoDimension');
         
         foreach (get_object_vars($struct) as $attr => $value) {
             if (substr($attr, -4) == 'Init') {
-                $this->$attr = self::unserializeObject($struct, $attr, ucfirst($attr));
+                $this->$attr = self::unserializeObject($struct, $attr, 
+                                   ucfirst($attr));
             }
         }
     }
@@ -220,7 +230,8 @@ class MapInfo extends Serializable {
         $childLayerId = $childLayer->id;
 
         if (in_array($childLayerId, array_keys($this->layers)))
-            throw new CartocommonException("Trying to replace layer $childLayerId");
+            throw new CartocommonException('Trying to replace layer ' .
+            $childLayerId);
 
         if (!in_array($childLayerId, $parentLayer->children))
             $parentLayer->children[] = $childLayerId;
