777fca0ce11ce7a1e6c02a144132d1f3cd576336
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2004-10-27 14:53:38 +0000

    Added automatic class icon support

--- server/MapInfoHandler.php
+++ server/MapInfoHandler.php
@@ -17,6 +17,11 @@ class MapInfoHandler {
     
     private $projectHandler;
 
+    private $iniPath;
+    private $symPath;
+    private $mapPath;
+    private $iconsPath;
+
     function __construct($serverContext, $mapId, $projectHandler) {
         $this->log =& LoggerManager::getLogger(__CLASS__);
         $this->serverContext = $serverContext;
@@ -39,12 +44,33 @@ class MapInfoHandler {
         return $mapInfo;
     }
 
+    private function getPath($storage, $dir, $ext = false) {
+        if(!isset($this->$storage)) {
+            $mapName = $this->projectHandler->getMapName();
+            $file = ($ext) ? ($mapName . '.' . $ext) : false;
+            $path = $this->projectHandler->getPath(CARTOSERVER_HOME,
+                        $dir . $mapName . '/', $file);
+            $this->$storage = CARTOSERVER_HOME . $path . $file;
+        }
+        return $this->$storage;
+    }
+    
     function getIniPath() {
+        return $this->getPath('iniPath', 'server_conf/', 'ini');
+   }
 
-        $mapName = $this->projectHandler->getMapName();
-        $path = $this->projectHandler->getPath(CARTOSERVER_HOME,
-                                'server_conf/' . $mapName . '/', $mapName . '.ini');
-        return CARTOSERVER_HOME . $path . $mapName . '.ini';
+    function getSymPath() {
+        return $this->getPath('symPath', 'server_conf/', 'sym');
+    }
+
+    function getIconsPath() {
+        return $this->getPath('iconsPath', 'www-data/icons/');
+    }
+
+    function getMapPath($serverContext) {
+        if (!isset($this->mapPath))
+            $this->mapPath = $serverContext->getMapPath();
+        return $this->mapPath;
     }
 
     private function loadMapInfo($mapId) {
@@ -76,6 +102,8 @@ class MapInfoHandler {
         $mapInfo = $this->mapInfo;
         $layers = $mapInfo->getLayers();
         $msMapObj = $serverContext->msMapObj;
+        
+        if ($mapInfo->autoClassLegend) $this->getMapPath($serverContext);
 
         foreach ($layers as $layer) {
             if (!$layer instanceof Layer)
@@ -95,7 +123,13 @@ class MapInfoHandler {
                     copy_vars($msClass, $layerClass);
                     $layerClass->id = $layer->id . '_class_' . $i;
                     $layerClass->label = utf8_encode($msClass->name);
-                
+               
+                    if ($mapInfo->autoClassLegend) {
+                        $layerClass->icon = $this->getClassIcon($layerClass->id, 
+                                                                $msMapObj,
+                                                                $msClass);
+                    }
+               
                     $mapInfo->addChildLayerBase($layer, $layerClass);
                 }
             }
@@ -124,6 +158,20 @@ class MapInfoHandler {
         $this->fillDynamicMap($serverContext);
         $this->fillDynamicLayers($serverContext);
         $this->fillDynamicKeymap($serverContext);
+    }
+
+    private function getClassIcon($classId, $msMapObj, $msClassObj) {
+        $classIcon = $this->getIconsPath() . $classId . '.png';
+        
+        if (!file_exists($classIcon) ||
+            filemtime($this->mapPath) > filemtime($classIcon) ||
+            filemtime($this->getSymPath()) > filemtime($classIcon)) {
+            $lgdIcon = $msClassObj->createLegendIcon($msMapObj->keysizex, 
+                                                     $msMapObj->keysizey);
+            if ($lgdIcon->saveImage($classIcon) < 0)
+                throw new CartoserverException("Failed writing $classIcon");
+        }
+        return basename($classIcon);
     }
 }
 ?>
