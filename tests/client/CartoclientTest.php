6599f28084c7a6e66558c2837269c515ac68e224
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2004-12-03 14:51:16 +0000

    added wsdl test

--- tests/client/CartoclientTest.php
+++ tests/client/CartoclientTest.php
@@ -16,6 +16,7 @@ require_once(CARTOCLIENT_HOME . 'client/Cartoclient.php');
  * Unit tests for Cartoclient.
  * @package Tests
  * @author Yves Bolognini <yves.bolognini@camptocamp.com>
+ * @author Sylvain Pasche <sylvain.pasche@camptocamp.com>
  */
 class client_CartoclientTest extends PHPUnit2_Framework_TestCase {
 
@@ -31,7 +32,7 @@ class client_CartoclientTest extends PHPUnit2_Framework_TestCase {
         return $url;
     }
     
-    public function testHttpUnit() {
+    private function getJavaPath() {
         
         // A list of path where to look for Java binary
         //  add your path here according to your system
@@ -48,8 +49,18 @@ class client_CartoclientTest extends PHPUnit2_Framework_TestCase {
 
         $ret = shell_exec("$java_path -version 2>&1");
         $this->assertContains("java version", $ret, 'You need a java virtual maching in your path');
+        
+        return $java_path;
+    }
+    
+    public function testHttpUnit() {
 
-        $httpunit_tests_path = realpath(dirname(__FILE__) . '/../httpunit');
+        $java_path = $this->getJavaPath();
+        
+        $ret = shell_exec("$java_path -version 2>&1");
+        $this->assertContains("java version", $ret, 'You need a java virtual maching in your path');
+
+        $httpunit_tests_path = realpath(dirname(__FILE__) . '/httpunit');
         
         $httpunit_jar = $httpunit_tests_path . '/httpunit_all.jar';
         $classpath = "$httpunit_jar:$httpunit_tests_path/bin";
@@ -65,6 +76,73 @@ class client_CartoclientTest extends PHPUnit2_Framework_TestCase {
         $this->assertContains("\nOK ", $java_output, "HttpUnit failure:\n $java_output");
     }
 
+    /**
+     * Delete a file, or a folder and its contents
+     *
+     * @author      Aidan Lister <aidan@php.net>
+     * @version     1.0.2
+     * @param       string   $dirname    Directory to delete
+     * @return      bool     Returns TRUE on success, FALSE on failure
+     */
+    function rmdirr($dirname)
+    {
+        // Sanity check
+        if (!file_exists($dirname)) {
+            return false;
+        }
+     
+        // Simple delete for a file
+        if (is_file($dirname)) {
+            return unlink($dirname);
+        }
+     
+        // Loop through the folder
+        $dir = dir($dirname);
+        while (false !== $entry = $dir->read()) {
+            // Skip pointers
+            if ($entry == '.' || $entry == '..') {
+                continue;
+            }
+     
+            // Recurse
+            $this->rmdirr("$dirname/$entry");
+        }
+     
+        // Clean up
+        $dir->close();
+        return rmdir($dirname);
+    }
+
+    private function getCartoserverBaseUrl() {
+        $ini_array = parse_ini_file(CARTOCLIENT_HOME . 'client_conf/client.ini');
+        return $ini_array['cartoserverBaseUrl'];
+    }
+                    
+    public function _testWsdl() {
+
+        $java_path = $this->getJavaPath();
+        $axis_tests_path = realpath(dirname(__FILE__) . '/axis');
+        $classpath = $axis_tests_path . '/axis.jar';
+        
+        $clientUrl = 'http://c2cpc4.camptocamp.com/gdp/1.0m7/cartoserver/cartoserver.wsdl.php?mapId=toposhop1'; 
+        //$clientUrl = $this->getCartoserverBaseUrl() . 'cartoserver.wsdl.php?mapId=test';
+
+        if (is_null($clientUrl)) {
+            $this->fail("Warning: client Url not found: skipping test\n");
+            return;   
+        }
+        $temp_dir = '/tmp/axis_tmp';
+        if (is_dir($temp_dir))
+            $this->rmdirr($temp_dir);
+        mkdir($temp_dir);
+        exec("$java_path -classpath $classpath " .
+                "org.apache.axis.wsdl.WSDL2Java " .
+                "-o $temp_dir -a $clientUrl 2>&1", $output, $ret);
+        $output = implode("\n", $output);
+        $this->rmdirr($temp_dir);
+        $output = "The wsdl file is invalid !!\n\n $output";
+        $this->assertTrue($ret == 0, $output);
+    }
 }
 
 ?>
