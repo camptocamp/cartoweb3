c0f64e450dfff1cf7150784ff0b1ac3c614e83f8
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2004-12-29 17:44:43 +0000

    updated adjustExportMapRequest()  method

--- coreplugins/location/client/ClientLocation.php
+++ coreplugins/location/client/ClientLocation.php
@@ -595,44 +595,53 @@ class ClientLocation extends ClientPlugin
     /**
      * @see Exportable::adjustExportMapRequest
      */
-     function adjustExportMapRequest(ExportConfiguration $configuration,
-                                     MapRequest $mapRequest) {
+    function adjustExportMapRequest(ExportConfiguration $configuration,
+                                    MapRequest $mapRequest) {
 
-         $locationRequest = $mapRequest->locationRequest;
+        $locationRequest = $mapRequest->locationRequest;
 
-         $type = $configuration->getLocationType();
-         if (!is_null($type)) {
-             $locationRequest->locationType = $type;
+        $type = $configuration->getLocationType();
+        if (!is_null($type)) {
+            $locationRequest->locationType = $type;
 
-             $locationType = $locationRequest->locationType;
-    
-             // reset every RelativeLocationRequest-typed properties:
-             foreach ($locationRequest as $member) {
-                 if ($member instanceof RelativeLocationRequest &&
-                     $member != ucfirst($locationType))
-                     unset($member);
-             }
-    
-             if ($locationType == 'zoomPointLocationRequest') {
-                 $bbox = $configuration->getBbox();
-                 if (!is_null($bbox))
-                     $locationRequest->$locationType->bbox = $bbox;
-        
-                 $point = $configuration->getPoint();
-                 if (!is_null($point))
-                     $locationRequest->$locationType->point = $point;
-        
-                 $scale = $configuration->getScale();
-                 if (!is_null($scale))
-                     $locationRequest->$locationType->scale = $scale;
-        
-                 $zoomType = $configuration->getZoomType();
-                 // FIXME: whtaever zoomType, we now use ZOOM_SCALE
-                 if (!is_null($zoomType))
-                     $locationRequest->$locationType->zoomType = 
-                         ZoomPointLocationRequest::ZOOM_SCALE;
-             }
-         }
-     }
+            $locationType = $locationRequest->locationType;
+  
+            $locationRequests = array('bboxLocationRequest',
+                                      'panLocationRequest', 
+                                      'zoomPointLocationRequest',
+                                      'recenterLocationRequest');
+            // FIXME: what if some new kind of request is added?
+            foreach ($locationRequest as $name => $member) {
+                if (in_array($name, $locationRequests)) 
+                    $locationRequest->$name = NULL;
+            }
+   
+            switch($locationType) {
+                case 'zoomPointLocationRequest':
+                    $locationRequest->$locationType = 
+                        new ZoomPointLocationRequest;
+                    
+                    $bbox = $configuration->getBbox();
+                    if (!is_null($bbox))
+                        $locationRequest->$locationType->bbox = $bbox;
+       
+                    $point = $configuration->getPoint();
+                    if (!is_null($point))
+                        $locationRequest->$locationType->point = $point;
+       
+                    $scale = $configuration->getScale();
+                    if (!is_null($scale))
+                        $locationRequest->$locationType->scale = $scale;
+       
+                    $zoomType = $configuration->getZoomType();
+                    // FIXME: use given zoomType instead of ZOOM_SCALE
+                    if (!is_null($zoomType))
+                        $locationRequest->$locationType->zoomType = 
+                            ZoomPointLocationRequest::ZOOM_SCALE;
+                    break;
+            
+            }
+        }
+    }
 }
 ?>
