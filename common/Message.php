4dacd81589d999cc37e89320c66f88b86079f18e
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2005-02-16 17:15:23 +0000

    added new attributes to messages: plugin, messageId

--- common/Message.php
+++ common/Message.php
@@ -13,26 +13,49 @@
  */
 class Message extends Serializable {
 
+    /**
+     * Constants to define the different the purpose of the message. Used for 
+     * instance to distinguate user and developer targeted messages.
+     */
     const CHANNEL_USER = 1;
     const CHANNEL_DEVELOPER = 2;
     
     /**
-     * @var string
+     * @var string The name of the plugin generation this message, or null if
+     * not created from a plugin
+     */
+    public $plugin;
+    
+    /**
+     * @var string Optional message identifier for machine parsing of messages.
+     * For instance, a message labeled "Too many results found", can have a 
+     * message identifier name "tooManyResults"
+     */
+    public $messageId;
+
+    /**
+     * @var string The text of the message to show to the user.
      */
     public $message;
     
     /**
-     * @var int  
+     * @var int The channel identifier of this message. See the constants named
+     * CHANNEL_... .
      */
     public $channel;
 
     /**
      * Constructor
-     * @param string
-     * @param int
+     * @param string the text of the message
+     * @param int the channel identifier of the message
+     * @param string optinal plugin name attached to this message
+     * @param string optional message identifier, for machine message parsing
      */
-    public function __construct($message = NULL, 
-                                $channel = self::CHANNEL_USER) {
+    public function __construct($message = NULL, $channel = self::CHANNEL_USER, 
+                                $plugin = NULL, $messageId = NULL) {
+        parent::__construct();
+        $this->plugin = $plugin;
+        $this->messageId = $messageId;
         $this->message = $message;
         $this->channel = $channel;
     }
@@ -41,6 +64,8 @@ class Message extends Serializable {
      * Sets object properties from $struct data.
      */
     public function unserialize($struct) {
+        $this->plugin = Serializable::unserializeValue($struct, 'plugin');
+        $this->messageId = Serializable::unserializeValue($struct, 'messageId');
         $this->message = Serializable::unserializeValue($struct, 'message');
         $this->channel = Serializable::unserializeValue($struct, 'channel', 
                                                         'int');
