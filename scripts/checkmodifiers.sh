0e573c39acc490f524a7f42b35c4ff13881e2971
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2005-02-18 16:18:10 +0000

    added

---
+++ scripts/checkmodifiers.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# TODO: add ../tests once modified
+find ../coreplugins ../plugins ../client ../common ../server ../projects ../htdocs -type f -a -iname "*php" |xargs egrep  '^[[:space:]]*function'
