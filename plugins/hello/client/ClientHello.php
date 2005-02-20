18070dd88cd85ef8a524ea650fbd83ff5f92a1d3
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2005-02-20 21:33:40 +0000

    improved doc and removed useless methods

--- plugins/hello/client/ClientHello.php
+++ plugins/hello/client/ClientHello.php
@@ -5,15 +5,27 @@
  */
 
 /**
+ * Demo plugin, shows how to output messages and use sessions.
  * @package Plugins
  */
 class ClientHello extends ClientPlugin
                   implements Sessionable, GuiProvider {
 
     const HELLO_INPUT = 'hello_input';
-    
+
+    /**
+     * @var Logger
+     */
     private $log;
+
+    /**
+     * @var string
+     */
     private $message;
+
+    /**
+     * @var int
+     */
     private $count;
 
     /** 
@@ -25,30 +37,49 @@ class ClientHello extends ClientPlugin
         $this->log =& LoggerManager::getLogger(__CLASS__);
     }
 
+    /**
+     * Retrieves count number from session.
+     * @see Sessionable::loadSession()
+     */
     public function loadSession($sessionObject) {
         $this->count = $sessionObject;
     }
 
+    /**
+     * Initializes session-saved var "count" to 0.
+     * @see Sessionable::createSession()
+     */
     public function createSession(MapInfo $mapInfo, InitialMapState $initialMapState) {
         $this->count = 0;
     }
+
+    /**
+     * Saves count number in session.
+     * @see Sessionable::saveSession()
+     */
     public function saveSession() {
         return $this->count;
     }
     
+    /**
+     * Increments count number and retrieves POST'ed message.
+     * @see GuiProvider::handleHttpPostRequest()
+     */
     public function handleHttpPostRequest($request) {
         $this->count = $this->count + 1;
         $this->message = @$_REQUEST[self::HELLO_INPUT];
     }
 
+    /**
+     * Not used.
+     * @see GuiProvider::handleHttpGetRequest()
+     */
     public function handleHttpGetRequest($request) {}
 
-    public function buildMapRequest($mapRequest) {
-        //$mapRequest->helloRequest = @$_REQUEST[self::HELLO_INPUT];
-    }
-
-    public function handleResult($result) {}
-
+    /**
+     * Draws plugins interface.
+     * @see GuiProvider::renderForm()
+     */
     public function renderForm(Smarty $template) {
 
         $template->assign('hello_active', true);
