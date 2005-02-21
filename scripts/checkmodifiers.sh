284e39358657b00963bad1c7aed3ac85323ca4c7
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2005-02-21 11:43:03 +0000

    skip some non object functions

--- scripts/checkmodifiers.sh
+++ scripts/checkmodifiers.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # TODO: add ../tests once modified
-find ../coreplugins ../plugins ../client ../common ../server ../projects ../htdocs -type f -a -iname "*php" |xargs egrep  '^[[:space:]]*function'
+find ../coreplugins ../plugins ../client ../common ../server ../projects ../htdocs -type f -a -iname "*php" |xargs egrep  '^[[:space:]]+function'\
+|grep -v 'Cartoserver.php:    function getMapInfo\|Cartoserver.php:    function getMap'
