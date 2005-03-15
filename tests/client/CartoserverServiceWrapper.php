8cad89d51b26254019d9e33d00139e20b4ef2166
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2005-03-15 16:34:12 +0000

    added file header doc

--- tests/client/CartoserverServiceWrapper.php
+++ tests/client/CartoserverServiceWrapper.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *
+ * CartoserverService wrapper for tests
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
