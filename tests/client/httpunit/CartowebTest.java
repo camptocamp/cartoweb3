15e72faeeb630e865b37506b6b80ad69d3961893
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2005-04-08 09:45:01 +0000

    new tests, added xhtml validation

--- tests/client/httpunit/CartowebTest.java
+++ tests/client/httpunit/CartowebTest.java
@@ -1,13 +1,21 @@
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import com.meterware.httpunit.Button;
 import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.HTMLElement;
 import com.meterware.httpunit.SubmitButton;
 import com.meterware.httpunit.WebConversation;
 import com.meterware.httpunit.WebForm;
-import com.meterware.httpunit.HTMLElement;
 import com.meterware.httpunit.WebRequest;
 import com.meterware.httpunit.WebResponse;
 
@@ -16,84 +24,173 @@ import com.meterware.httpunit.WebResponse;
  * 
  * @author sypasche
  */
-public class CartowebTest extends TestCase {
+public class CartowebTest extends TestCase
+{
 
-    static String URL;
-    
-    public CartowebTest(String name) {
-        super(name);
-    }
+	/** Directory where to save html fetched files */
+	static final String HTML_SAVE_DIRECTORY = "/tmp/validator_tmp/";
 
-    public static void main(String args[]) {        
-        if (args.length != 1) {
-            System.out.println("Usage: CartowebTest CARTOCLIENT_URL\n");
-            System.exit(0);
-        }
-        CartowebTest.URL = args[0];
+	static String URL;
 
-        junit.textui.TestRunner.run(suite());
-    }
-    
-    public static Test suite() {
-        if (URL == null)
-            URL = System.getProperty("cartoclient_url");
-        if (URL == null)
-            throw new RuntimeException("Please invoke this test via main(), with URL argument");
-        
-        return new TestSuite(CartowebTest.class);
-    }
+	public CartowebTest( String name )
+	{
+		super( name );
+	}
 
-    private String getProjectUrl(String projectId) {
-                
-        return URL + "?project=" + projectId;
-    }
-    
-    private void assertContainsMainmap(WebResponse response) throws Exception {
-        
-        HTMLElement mainmap = response.getElementWithID("mapImageDiv");
-        assertNotNull("No mainmap image on cartoclient page", mainmap);
-    }
-    
-    private void testMainmapPresent(String projectId) throws Exception {
-        
-        WebConversation     conversation = new WebConversation();
-        WebRequest request = new GetMethodWebRequest(getProjectUrl(projectId));
-        WebResponse response = conversation.getResponse(request);
-        
-        assertContainsMainmap(response);
-    }
+	public static void main( String args[] )
+	{
+		if ( args.length != 1 )
+		{
+			System.out.println( "Usage: CartowebTest CARTOCLIENT_URL\n" );
+			System.exit( 0 );
+		}
+		CartowebTest.URL = args[ 0 ];
 
-    private void testKeymap(String projectId) throws Exception {
-                            
-        WebConversation     conversation = new WebConversation();
-        WebResponse response = conversation.getResponse(getProjectUrl(projectId));
-                
-        assertContainsMainmap(response);
-                
-        // click on the center of the keymap
-        SubmitButton keymapButton = response.getFormWithName("carto_form").getSubmitButton("keymap");
-        keymapButton.click(50, 50);
-        assertContainsMainmap(conversation.getCurrentPage());
-    }
-    
-    private void testProject(String projectId) throws Exception {
-        testMainmapPresent("default");
-        testKeymap("default");
-    }
-    
-    public void testDefaultProject() throws Exception {
-        testProject("default");
-    }
+		junit.textui.TestRunner.run( suite() );
+	}
 
-    public void testTestprojectProject() throws Exception {
-        String projectId = "testproject";
-        testProject(projectId);
-                        
-        WebConversation conversation = new WebConversation();
-        WebResponse response = conversation.getResponse(getProjectUrl(projectId));
-        assertTrue("Testproject title does not contain \"test_project\"", 
-                   response.getElementWithID("banner").getText().indexOf("testproject") != -1);
-                        
-    }
+	public static Test suite()
+	{
+		if ( URL == null ) URL = System.getProperty( "cartoclient_url" );
+		if ( URL == null ) throw new RuntimeException( "Please invoke this test via main(), with URL argument" );
 
+		return new TestSuite( CartowebTest.class );
+	}
+
+	/**
+	 * Saves a response html into a directory.
+	 * 
+	 * @param currentPage
+	 * @param string
+	 */
+	private void saveAs( WebResponse currentPage, String string ) throws Exception
+	{
+		String outputFilename = HTML_SAVE_DIRECTORY + string;
+		File f = new File( outputFilename );
+		OutputStream os = new FileOutputStream( f );
+		os.write( currentPage.getText().getBytes() );
+		os.close();
+	}
+
+	/**
+	 * Checks that the html generated response is Xhtml valid.
+	 * 
+	 * @param response
+	 * @throws Exception
+	 */
+	private void checkXHTMLValidity( WebResponse response ) throws Exception
+	{
+
+		Process process = null;
+		try
+		{
+			process = Runtime.getRuntime().exec(
+					new String[] { "xmllint", "--valid", "--nonet", "--noout", "--postvalid", "-" } );
+		}
+		catch ( IOException ioe )
+		{
+			fail( "Problem which launching validator, please check that it is installed correctly:\n" + ioe );
+			return;
+		}
+
+		OutputStream outStream = new BufferedOutputStream( process.getOutputStream() );
+
+		outStream.write( response.getText().getBytes() );
+		outStream.close();
+		process.waitFor();
+
+		InputStream errorInputStream = new BufferedInputStream( process.getErrorStream() );
+		byte[] buffer = new byte[ 1024 ];
+		int read = errorInputStream.read( buffer );
+		if ( read > 0 )
+		{
+			String str = new String( buffer, 0, read );
+			System.out.println( "Validation result:" );
+			System.out.println( str );
+			errorInputStream.close();
+		}
+		assertEquals( "Validation failed, please look at the above message", 0, process.exitValue() );
+	}
+
+	/**
+	 * Returns the URL to access CartoWeb using a project
+	 * 
+	 * @param projectId
+	 * @return
+	 */
+	private String getProjectUrl( String projectId )
+	{
+
+		return URL + "?project=" + projectId;
+	}
+
+	private void assertContainsMainmap( WebResponse response ) throws Exception
+	{
+
+		HTMLElement mainmap = response.getElementWithID( "mapImageDiv" );
+		saveAs( response, "last_checked_page.html" );
+		assertNotNull( "No mainmap image on cartoclient page", mainmap );
+		checkXHTMLValidity( response );
+	}
+
+	private void testProject( WebConversation conversation, String projectId ) throws Exception
+	{
+
+		assertContainsMainmap( conversation.getCurrentPage() );
+		WebResponse response = conversation.getCurrentPage();
+
+		// click on the center of the keymap
+		SubmitButton keymapButton = response.getFormWithName( "carto_form" ).getSubmitButton( "keymap" );
+		keymapButton.click( 50, 50 );
+		assertContainsMainmap( conversation.getCurrentPage() );
+	}
+
+	public void testDefaultProject() throws Exception
+	{
+		String projectId = "default";
+		WebConversation conversation = new WebConversation();
+
+		WebRequest request = new GetMethodWebRequest( getProjectUrl( projectId ) );
+		WebResponse response = conversation.getResponse( request );
+
+		saveAs( response, "first_display.html" );
+		assertContainsMainmap( response );
+
+		WebForm cartoForm = response.getFormWithName( "carto_form" );
+		SubmitButton dummyButton = cartoForm.getSubmitButtons()[ 0 ];
+		WebRequest webRequ = cartoForm.newUnvalidatedRequest( dummyButton );
+
+		webRequ.setParameter( "tool", "query" );
+		webRequ.setParameter( "selection_type", "rectangle" );
+		webRequ.setParameter( "selection_coords", "204,109;204,109" );
+
+		response = conversation.getResponse( webRequ );
+		saveAs( response, "query_result.html" );
+		assertContainsMainmap( response );
+
+		testProject( conversation, projectId );
+	}
+
+	public void testTestprojectProject() throws Exception
+	{
+		String projectId = "testproject";
+		WebConversation conversation = new WebConversation();
+		WebRequest request = new GetMethodWebRequest( getProjectUrl( projectId ) );
+		WebResponse response = conversation.getResponse( request );
+		testProject( conversation, projectId );
+		response = conversation.getResponse( getProjectUrl( projectId ) );
+		assertTrue( "Testproject title does not contain \"test_project\"", response.getElementWithID( "banner" ).getText()
+				.indexOf( "testproject" ) != -1 );
+
+	}
+
+	// FIXME: enable this once the demo project is xhtml valid
+	public void OFF_testDemoProject() throws Exception
+	{
+		String projectId = "demo";
+		WebConversation conversation = new WebConversation();
+		WebRequest request = new GetMethodWebRequest( getProjectUrl( projectId ) );
+		WebResponse response = conversation.getResponse( request );
+		testProject( conversation, projectId );
+	}
 }
