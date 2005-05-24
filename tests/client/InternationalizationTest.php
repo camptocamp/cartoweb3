69efe0cdc45eef20ad468781ae3bb5206f720f91
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2005-05-24 10:47:40 +0000

    added

---
+++ tests/client/InternationalizationTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * I18n tests
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @copyright 2005 Camptocamp SA
+ * @package Tests
+ * @version $Id$
+ */
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+// This is currently not included in tests. Add to the tests once completed.
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+/**
+ * Abstract test case
+ */
+require_once 'PHPUnit2/Framework/TestCase.php';
+require_once 'common/Config.php';
+
+require_once(CARTOCLIENT_HOME . 'client/Internationalization.php');
+require_once(CARTOCLIENT_HOME . 'client/Cartoclient.php');
+
+/**
+ * Unit tests for Internationalization
+ * @package Tests
+ * @author Sylvain Pasche <sylvain.pasche@camptocamp.com>
+ */
+class client_InternationalizationTest extends PHPUnit2_Framework_TestCase {
+
+    public function testGettextFr() {
+        $cartoclient = new Cartoclient();
+        $config = new ClientConfig($cartoclient->getProjectHandler());
+        I18n::init($config);
+        
+        //var_dump(I18n::getLocales());
+        $translated = I18n::gt('Scalebar');
+        $this->assertEquals('Echelle', $translated);
+    }
+}
+
+?>
