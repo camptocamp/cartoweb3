a9c4b387dc331ed367b89dc87f1db9fe5b4950cd
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2005-07-04 14:44:59 +0000

    check .map and .sym too

--- scripts/checktabs.sh
+++ scripts/checktabs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find .. -type f -a \( -name '*dist' -o  -name '*php' -o -name '*tpl' -o -name '*wsdl' -o -name '*xml' \) \
+find .. -type f -a \( -name '*dist' -o  -name '*php' -o -name '*tpl' -o -name '*wsdl' -o -name '*xml' -o -name "*map" -o -name "*sym" \) \
 |xargs grep -l '	'|grep -v include/pear|grep -v '/include/'
