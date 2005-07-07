f321510f46fe6cc5e93068ab697a4720e430d258
afellay <afellay@4943411f-c6a7-0410-800e-c336a95043a2>
2005-07-07 07:44:48 +0000

    Encoding issues (one more time)

--- coreplugins/tables/client/ClientTables.php
+++ coreplugins/tables/client/ClientTables.php
@@ -152,9 +152,9 @@ class ClientTables extends ClientPlugin
             }            
             foreach ($tableGroup->tables as $table) {
                 if (!empty($table->tableTitle))
-                    $table->tableTitle = I18n::gt($table->tableTitle);
+                    $table->tableTitle = Encoder::encode(I18n::gt($table->tableTitle));
                 foreach ($table->columnTitles as $key => $columnTitle) {
-                    $table->columnTitles[$key] = I18n::gt($columnTitle);                          
+                    $table->columnTitles[$key] =  Encoder::encode(I18n::gt($columnTitle));                          
                 }
                 if ($table->numRows == 0) {
                     continue;
