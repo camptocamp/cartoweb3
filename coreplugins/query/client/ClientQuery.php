55eaf9f8029123ef3a29020aa16071eb270edfea
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2005-08-15 16:36:09 +0000

    added missing document title (header comments)

--- coreplugins/query/client/ClientQuery.php
+++ coreplugins/query/client/ClientQuery.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *
+ * Client query plugin
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
