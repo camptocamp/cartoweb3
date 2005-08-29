859ddf9d943df5f7caabc350c585d4e10069775e
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2005-08-29 14:25:56 +0000

    added .cpp, .c and README files

--- scripts/checktabs.sh
+++ scripts/checktabs.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-find .. -type f -a \( -name '*dist' -o  -name '*php' -o -name '*tpl' -o -name '*wsdl' -o -name '*xml' -o -name "*map" -o -name "*sym" \) \
-|xargs grep -l '	'|grep -v include/pear|grep -v '/include/'
+find .. -type f -a \( -name '*dist' -o  -name '*php' -o -name '*tpl' -o -name '*wsdl' \
+     -o -name '*xml' -o -name "*map" -o -name "*sym" -o -name "*.c" -o -name "*cpp" -o -name "*README*" \) \
+|xargs grep -l '	'|grep -v include/pear|grep -v '/include/'|grep -v 'vera/README.TXT'
