96e797fc62f5574ce9893ca62889b363ea7a4e12
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2005-10-10 14:18:37 +0000

    info.php will display a WARNING message if not able to load mapscript

--- htdocs/info.php
+++ htdocs/info.php
@@ -4,6 +4,18 @@
  * @version $Id$
  */
 
+if (!extension_loaded('mapscript')) {
+    $prefix = (PHP_SHLIB_SUFFIX == 'dll') ? '' : 'php_';
+    if (!dl($prefix . 'mapscript.' . PHP_SHLIB_SUFFIX))
+        print("WARNING: can't load mapscript library");
+} else if (substr(php_sapi_name(), 0, 3) != 'cgi') {
+    print("WARNING: You are not using PHP as " .
+        "a cgi and PHP Mapscript extension is loaded in your " .
+        "php.ini.\n This will cause stability problems.\n" .
+        "You need to remove the " .
+        "php_mapscript extension loading of your php.ini " .
+        "file.");
+}
 phpinfo();
 
 ?>
