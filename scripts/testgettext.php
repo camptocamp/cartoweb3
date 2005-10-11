5128b7e55f8c1428a186cdfd09ffb44af750ab22
mbornoz <mbornoz@4943411f-c6a7-0410-800e-c336a95043a2>
2005-10-11 06:45:11 +0000

    Scripts permettant de tester le bon fonctionnement de gettext que ce soit au niveau du syst�me avec testgettext.sh ou encore au niveau de PHP avec testgettext.php. Pour l'instant ces scripts ne fonctionnent que sous Linux.

---
+++ scripts/testgettext.php
@@ -0,0 +1,62 @@
+<?php
+
+/** 
+ * Little script to test if PHP-Gettext 
+ * is correctly installed.
+ *
+ * This script run, at this time, only on Linux
+ *
+ */
+
+$MAIN = "sample";
+$LOCALE = "fr";
+$LOCALE_PATH = "/tmp";
+
+system("mkdir -p $LOCALE_PATH/$LOCALE/LC_MESSAGES", $error);
+if ($error) {
+    echo "error: create $LOCALE_PATH/$LOCALE/LC_MESSAGES folder failed !\n";
+    die();
+}
+
+$po = "msgid \"\"\n" .
+      "msgstr \"\"\n" .
+      "\"Project-Id-Version: \\n\"\n" .
+      "\"POT-Creation-Date: 2005-07-21 10:05+0000\\n\"\n" .
+      "\"PO-Revision-Date: 2005-06-15 15:58+0100\\n\"\n" .
+      "\"Last-Translator: Mathieu Bornoz <mathieu.bornoz@camptocamp.com>\\n\"\n" .
+      "\"Language-Team: \\n\"\n" .
+      "\"MIME-Version: 1.0\\n\"\n" .
+      "\"Content-Type: text/plain; charset=ISO-8859-1\\n\"\n" .
+      "\"Content-Transfer-Encoding: 8bit\\n\"\n" .
+      "msgid \"PHP-Gettext is not installed correctly !\"\n" .
+      "msgstr \"PHP-Gettext is OK, well done !\"";
+
+$file = "$LOCALE_PATH/$MAIN.po";   
+if (!$file_handle = fopen($file,"w")) { 
+    echo "error : cannot create file $file"; 
+    die();
+}  
+if (!fwrite($file_handle, $po)) { 
+    echo "error: cannot write to file $file"; 
+    die();
+}  
+fclose($file_handle);   
+
+system("msgfmt -o $LOCALE_PATH/$LOCALE/LC_MESSAGES/$MAIN.mo $file", $error);
+if ($error) {
+    echo "error: create sample.mo failed with command msgfmt !\n";
+    die();
+}
+
+setlocale(LC_ALL, $LOCALE);
+putenv("LANG=$LOCALE"); 
+bindtextdomain($MAIN, $LOCALE_PATH); 
+textdomain($MAIN);
+printf("<pre><b>%s</b></pre>\n", gettext("PHP-Gettext is not installed correctly !"));
+
+system("rm -fR $LOCALE_PATH/$LOCALE $file", $error);
+if ($error) {
+    echo "error: can't delete temp files !\n";
+    die();
+}
+?>
---
+++ scripts/testgettext.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+# 
+# Little C program compiled on the fly to test 
+# if gettext is correctly installed
+# 
+# This script needs gettext, gcc, libc6-dev and run, 
+# at this time, only on Linux.
+#
+
+MAIN="sample"
+LOCALE="fr"
+LOCALE_PATH="/tmp"
+
+cat > $LOCALE_PATH/$MAIN.c <<EOF
+/*
+ * Sample program for testing the gettext
+ * File  : sample.c
+ */
+
+#include<stdio.h>    /* Standard Header File */
+
+/* Header Files required for gettext support */
+#include<locale.h>   /* Definition of Locale Variables LC_*  */
+#include<libintl.h>  /* Function definitions for NLS */
+#include <stdlib.h>
+
+/*
+ * Macros for reducing the typing
+ * Macros are understandable by the gettext preprocessor
+ */
+#define _(String) gettext(String)
+#define gettext_noop(String) (String)
+
+main(){
+
+    char message[] = gettext_noop("Gettext is not installed correctly !");
+
+    /* Reset the locale variables LC_* */
+    setlocale(LC_ALL, "");
+    setlocale(LC_TIME, "" );
+    setlocale(LC_MESSAGES, "");
+
+    /* Bind with MO File */
+    bindtextdomain( "$MAIN", "$LOCALE_PATH");
+    textdomain( "$MAIN") ;
+
+    printf("%s\n", _(message));
+
+    return 0;
+}
+EOF
+
+xgettext -ao $LOCALE_PATH/$MAIN-tmp.po $LOCALE_PATH/$MAIN.c > /dev/null 2>&1
+head -n 22 $LOCALE_PATH/$MAIN-tmp.po |sed -e 's/""/"Gettext is OK, well done !"/g' -e 's/CHARSET/ISO-8859-1/g' > $LOCALE_PATH/$MAIN.po
+
+mkdir -p $LOCALE_PATH/$LOCALE/LC_MESSAGES
+msgfmt -o $LOCALE_PATH/$LOCALE/LC_MESSAGES/$MAIN.mo $LOCALE_PATH/$MAIN.po
+
+#Compile the program
+gcc -o $LOCALE_PATH/$MAIN $LOCALE_PATH/$MAIN.c
+    
+# Now set [fr] as default language
+export LANG=$LOCALE
+
+# Run the program
+$LOCALE_PATH/sample
+
+# Clean all
+rm -f $LOCALE_PATH/sample*
+rm -fR $LOCALE_PATH/$LOCALE
+
+exit 0
