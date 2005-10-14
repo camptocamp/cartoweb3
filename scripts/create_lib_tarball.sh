06318d389f899b81185b2b585d7696f1e7da3d06
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2005-10-14 12:47:54 +0000

    new versions

--- scripts/create_lib_tarball.sh
+++ scripts/create_lib_tarball.sh
@@ -5,7 +5,32 @@
 # 
 # You need to adjust the URLs of the libraries if you need a newer version
 
-PEAR_PACKAGES="Benchmark PHPUnit2 PhpDocumentor PEAR DB Archive_Tar XML_RPC Console_Getopt Auth HTML_Crypt"
+set -e
+
+PEAR_PACKAGES_STABLE="Benchmark PHPUnit2 PEAR DB Archive_Tar XML_RPC Console_Getopt Auth HTML_Crypt"
+PEAR_PACKAGES_DEVEL="PhpDocumentor"
+
+# Dependencies:
+#
+#Benchmark
+#
+#PHPUnit2
+#    Console_Getopt
+#    PEAR
+#
+#PhpDocumentor 
+#    Archive_Tar
+#PEAR
+#    Archive_Tar
+#    XML_RPC
+#DB
+#    PEAR
+#Archive_Tar
+#XML_RPC
+#Console_Getopt
+#Auth
+#HTML_Crypt
+
 
 LOG4PHP="http://www.vxr.it/log4php/log4php-0.9.tar.gz"
 SMARTY="http://smarty.php.net/do_download.php?download_file=Smarty-2.6.10.tar.gz"
@@ -28,11 +53,27 @@ fetch_pear()
 
     # Warning: please remove preferred_state=devel when phpDocumentor is php 5 compatible
 
-    pear -s -c $PEAR_DIRECTORY/.pearrc -d preferred_state=devel -d doc_dir=$PEAR_DIRECTORY/docs -d ext_dir=$PEAR_DIRECTORY/ext \
+    pear -s -c $PEAR_DIRECTORY/.pearrc -d doc_dir=$PEAR_DIRECTORY/docs -d ext_dir=$PEAR_DIRECTORY/ext \
            -d php_dir=$PEAR_PHP_DIR -d data_dir=$PEAR_DIRECTORY/data -d test_dir=$PEAR_DIRECTORY/tests \
            -d cache_dir=$PEAR_DIRECTORY/cache -d bin_dir=$PEAR_DIRECTORY/bin 
           
-    pear -c $PEAR_DIRECTORY/.pearrc install $PEAR_PACKAGES
+    ## BUG: if executed all at once without the --nodeps flag, pear will get into an infinite recursion, and make php segfault !
+    for i in $PEAR_PACKAGES_STABLE; do 
+        pear -c $PEAR_DIRECTORY/.pearrc install --nodeps $i
+    done
+
+    # ... use this instead if pear is fixed
+    #pear -c $PEAR_DIRECTORY/.pearrc install --nodeps $PEAR_PACKAGES
+
+    pear -s -c $PEAR_DIRECTORY/.pearrc -d preferred_state=devel -d doc_dir=$PEAR_DIRECTORY/docs -d ext_dir=$PEAR_DIRECTORY/ext \
+           -d php_dir=$PEAR_PHP_DIR -d data_dir=$PEAR_DIRECTORY/data -d test_dir=$PEAR_DIRECTORY/tests \
+           -d cache_dir=$PEAR_DIRECTORY/cache -d bin_dir=$PEAR_DIRECTORY/bin 
+
+    for i in $PEAR_PACKAGES_DEVEL; do 
+        pear -c $PEAR_DIRECTORY/.pearrc install --nodeps $i
+    done
+
+    
 }
 
 fetch_contrib()
@@ -81,4 +122,4 @@ prepare
 (cd include && fetch_pear)
 (cd include && fetch_contrib)
 create_tarball
-upload
+#upload
