849b5e486acab6b1e8533c07abcdff038315ce89
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2005-11-03 17:34:02 +0000

    add

---
+++ projects/demoCW3/tests/client/AllTests.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @copyright 2005 Camptocamp SA
+ * @package Tests
+ * @version $Id$
+ */
+
+/**
+ * Abstract test suite
+ */
+require_once 'PHPUnit2/Framework/TestSuite.php';
+
+/**
+ * All client tests
+ */
+require_once(CARTOWEB_HOME . 'projects/demoCW3/tests/client/CartoclientTest.php');
+
+/**
+ * @package Tests
+ * @author Sylvain Pasche <sylvain.pasche@camptocamp.com>
+ */
+class projects_demoCW3_client_AllTests {
+    
+    public static function suite() {
+
+        $suite = new PHPUnit2_Framework_TestSuite;
+
+        $suite->addTestSuite('projects_demoCW3_client_CartoclientTest');
+
+        return $suite;
+    }
+}
+
+?>
