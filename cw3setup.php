22fd3ce6574eeb190744f79e37e66dd264cbdea2
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2005-11-09 15:59:04 +0000

    typo

--- cw3setup.php
+++ cw3setup.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Cartoweb3 installer
+ * CartoWeb3 Installer
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
