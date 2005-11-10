9c1b39d1560f1789dc7b7259bc352f016e37282b
daniel <daniel@4943411f-c6a7-0410-800e-c336a95043a2>
2005-11-10 15:23:58 +0000

    Added  copy of pHp-CGI

--- cw3post_setup.bat
+++ cw3post_setup.bat
@@ -1,1 +1,3 @@
 %1\php\php cw3setup.php --install --base-url http://127.0.0.1/cartoweb3/htdocs/ --profile production
+copy %1\php\php-cgi.exe %1\Apache\cgi-bin\
+del %1\www\cartoweb3\cw3post_setup.bat
