29c0804043cc64ccb89ae829138c88d916b10928
daniel <daniel@4943411f-c6a7-0410-800e-c336a95043a2>
2005-11-16 16:12:51 +0000

    Updated for cgi mode

--- cw3post_setup.bat
+++ cw3post_setup.bat
@@ -1,3 +1,2 @@
-%1\php\php cw3setup.php --install --base-url http://127.0.0.1/cartoweb3/htdocs/ --profile production
-copy %1\php\php-cgi.exe %1\Apache\cgi-bin\
+%1\php\php -n cw3setup.php --install --base-url http://127.0.0.1/cartoweb3/htdocs/ --profile production
 del %1\www\cartoweb3\cw3post_setup.bat
