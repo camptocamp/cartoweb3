030be9f00e9f1709ec82d7e42be3f9cba5b84977
fredj <fredj@4943411f-c6a7-0410-800e-c336a95043a2>
2005-12-01 09:38:20 +0000

    added invert_image function

--- common/Utils.php
+++ common/Utils.php
@@ -176,6 +176,75 @@ class Utils {
         $value = explode(',', $value);
         return array_map('trim', $value);
     }
+
+    /**
+     * Invert and save image
+     * @param string
+     * @param string
+     * @param boolean
+     * @param string jpeg or png
+     * @return array
+     */
+    static public function invert_image($input, $output, $color=false, $type='jpeg') {
+        // $input = 'path/to/image.jpg';
+        
+        // for a color negative image, set the optional flag
+        // invert_image($input, '', true);
+
+        // for a black and withe negative image use like this
+        //
+        // invert_image($input, '');
+
+        // if you want to save the output instead of just showing it,
+        // set the output to the path where you want to save the inverted image
+        //
+        // invert_image('path/to/original/image.jpg','path/to/save/inverted-image.jpg');
+
+        // if you want to use png you have to set the color flag as
+        // true or false and define the imagetype in the function call
+        //
+        // invert_image('path/to/image.png','',false,'png');
+    
+        switch ($type) {
+        case 'jpeg':
+            $bild = imagecreatefromjpeg($input);
+            break;
+        case 'png':
+            $bild = imagecreatefrompng($input);
+            break;
+        default:
+            throw new CartocommonException("$type is not a valid image type");
+            break;
+        }
+
+        $x = imagesx($bild);
+        $y = imagesy($bild);
+
+        for($i=0; $i < $y; $i++) {
+            for($j=0; $j < $x; $j++) {
+                $pos = imagecolorat($bild, $j, $i);
+                $f = imagecolorsforindex($bild, $pos);
+                if($color == true) {
+                    $col = imagecolorresolve($bild, 255-$f['red'], 255-$f['green'], 255-$f['blue']);
+                } else {
+                    $gst = $f['red']*0.15 + $f['green']*0.5 + $f['blue']*0.35;
+                    $col = imagecolorclosesthwb($bild, 255-$gst, 255-$gst, 255-$gst);
+                }
+                imagesetpixel($bild, $j, $i, $col);
+            }
+        }
+        switch ($type) {
+        case 'jpeg':
+            imagejpeg($bild, $output, 90);
+            break;
+        case 'png':
+            imagepng($bild, $output);
+            break;
+        default:
+            throw new CartocommonException("$type is not a valid image type");
+            break;
+        }
+    }
 }
 
 /**
