b696b20c684d52dbc1ff6d4874d4b43c1b76381c
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2005-12-21 13:31:57 +0000

    updated smarty version

--- scripts/create_lib_tarball.sh
+++ scripts/create_lib_tarball.sh
@@ -33,7 +33,7 @@ PEAR_PACKAGES_DEVEL="PhpDocumentor"
 
 
 LOG4PHP="http://www.vxr.it/log4php/log4php-0.9.tar.gz"
-SMARTY="http://smarty.php.net/do_download.php?download_file=Smarty-2.6.10.tar.gz"
+SMARTY="http://smarty.php.net/do_download.php?download_file=Smarty-2.6.11.tar.gz"
 FPDF="http://www.fpdf.org/fr/dl.php?v=153&f=tgz"
 
 # uncomment to upload with scp to this address
