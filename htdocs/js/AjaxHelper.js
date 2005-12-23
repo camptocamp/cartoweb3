691ce503125d17d25fbecc5e4fdb0fcfabce9270
fredj <fredj@4943411f-c6a7-0410-800e-c336a95043a2>
2005-12-23 10:32:16 +0000

    indent, coding style

--- htdocs/js/AjaxHelper.js
+++ htdocs/js/AjaxHelper.js
@@ -18,15 +18,17 @@ var AjaxHelper = {
      */
     buildQueryFrom: function(htmlElement) {
     	
-    	if (htmlElement == undefined)
-    		return;
+    	if (htmlElement == undefined) {
+            return;
+        }
     	
     	inputType = htmlElement.getAttribute('type');
 	    paramName = htmlElement.getAttribute('name');
-	    if (inputType == 'text')
-	    	paramValue = htmlElement.value
-	    else
-	    	paramValue = htmlElement.getAttribute('value')
+	    if (inputType == 'text') {
+            paramValue = htmlElement.value;
+        } else {
+            paramValue = htmlElement.getAttribute('value');
+        }
 	    	
 	    if (paramValue != null) {
      		return paramName + '=' + paramValue;
@@ -55,22 +57,19 @@ var AjaxHelper = {
          * Process <input> elements
          */
         inputElements = formElement.getElementsByTagName('input');
-        for (var i=0; i < inputElements.length; i++) {
+        for (var i = 0; i < inputElements.length; i++) {
             // Collect the current input only if it's is not 'submit', 'image' or 'button'
             currentElement = inputElements.item(i);
             inputType = currentElement.getAttribute('type');
             
             if (inputType == 'radio' || inputType == 'checkbox') {
-                if (currentElement.checked)
+                if (currentElement.checked) {
                     queryString = queryString + this.buildQueryFrom(currentElement) + '&';
-                    
-            } else if (inputType == 'submit'
-            			|| inputType == 'button' 
-            			|| inputType == 'image') {
+                }
+            } else if (inputType == 'submit' || inputType == 'button' || inputType == 'image') {
                 // Do nothing. Sending the submit inputs in POST Request would make
                 // the serverside act like all buttons on the form were clicked.
                 // And we don't want that.
-                
             } else {    
                 queryString = queryString + this.buildQueryFrom(currentElement) + '&';
             }
@@ -80,7 +79,7 @@ var AjaxHelper = {
          * Process <select> elements
          */
         selectElements = formElement.getElementsByTagName('select');
-        for (var i=0; i < selectElements.length; i++) {
+        for (var i = 0; i < selectElements.length; i++) {
             // Get the param name (i.e. fetch the name attr)
             currentElement = selectElements.item(i);
             paramName = currentElement.getAttribute('name');
@@ -88,20 +87,19 @@ var AjaxHelper = {
             // Get the param value(s)
             // (i.e. fetch the checked options element's value attr)
             optionElements = currentElement.getElementsByTagName('option');
-            for (var j=0; j < optionElements.length; j++) {
+            for (var j = 0; j < optionElements.length; j++) {
                 currentElement = optionElements.item(j);
                 if (currentElement.selected) {
 					paramValue = currentElement.getAttribute('value');
-                    if (paramValue == null)
+                    if (paramValue == null) {
                     	paramValue = '';
-                    queryString = queryString + paramName + '='
-                    	+ paramValue + '&';
+                    }
+                    queryString = queryString + paramName + '='	+ paramValue + '&';
                 }
             }
         }
         return queryString;
     },
-
             
     /**
      * Returns the query string contained in the href attribute
@@ -110,17 +108,17 @@ var AjaxHelper = {
      * @return string HTTP GET string from ahrefElement
      */
     getQueryString: function(ahrefElement) {
-    	if (ahrefElement == undefined)
-    		return;
+    	if (ahrefElement == undefined) {
+            return;
+        }
         // Retrieves the string after the '?' of the element's href attribute.
         startChar = ahrefElement.indexOf('?');
         endChar = ahrefElement.length;
         if (startChar != '-1') {
-            var queryString = ahrefElement.substring(startChar+1, endChar);
+            return ahrefElement.substring(startChar+1, endChar);
         } else {
-            var queryString = '';
+            return '';
         }
-        return queryString;
     },
 	
 	/**
@@ -134,17 +132,18 @@ var AjaxHelper = {
 	 * @param bool Prevent event from bubbling through the DOM
 	 */
 	addEvent: function (elm, evType, fn, useCapture) {
-	  if (useCapture == undefined)
-	  	useCapture = false;
-	  if (elm.addEventListener) {
-	    elm.addEventListener(evType, fn, useCapture);
-	    return true;
-	  } else if (elm.attachEvent) {
-	    var r = elm.attachEvent('on' + evType, fn);
-	    return r;
-	  } else {
-	    elm['on' + evType] = fn;
-	  }
+        if (useCapture == undefined) {
+            useCapture = false;
+        }
+        if (elm.addEventListener) {
+            elm.addEventListener(evType, fn, useCapture);
+            return true;
+        } else if (elm.attachEvent) {
+            var r = elm.attachEvent('on' + evType, fn);
+            return r;
+        } else {
+            elm['on' + evType] = fn;
+        }
 	},
 	
 	getCurrentTool: function() {
@@ -177,35 +176,30 @@ var AjaxHelper = {
 	 */
 	getClickedPos: function(ev) {
 	
-		function findPosX(obj)
-		{
+		function findPosX(obj) {
 			var curleft = 0;
-			if (obj.offsetParent)
-			{
-				while (obj.offsetParent)
-				{
-					curleft += obj.offsetLeft
-					obj = obj.offsetParent;
-				}
-			}
-			else if (obj.x)
-				curleft += obj.x;
+			if (obj.offsetParent) {
+				while (obj.offsetParent) {
+                    curleft += obj.offsetLeft;
+                    obj = obj.offsetParent;
+                }
+			} else if (obj.x) {
+                curleft += obj.x;
+            }
 			return curleft;
 		}
 		
-		function findPosY(obj)
-		{
+		function findPosY(obj) {
 			var curtop = 0;
-			if (obj.offsetParent)
-			{
-				while (obj.offsetParent)
-				{
-					curtop += obj.offsetTop
-					obj = obj.offsetParent;
-				}
+			if (obj.offsetParent) {
+				while (obj.offsetParent){
+                    curtop += obj.offsetTop;
+                    obj = obj.offsetParent;
+                }
 			}
-			else if (obj.y)
-				curtop += obj.y;
+			else if (obj.y) {
+                curtop += obj.y;
+            }
 			return curtop;
 		}
 	
@@ -224,9 +218,7 @@ var AjaxHelper = {
 		mX += document.body.scrollLeft;
 		mY += document.body.scrollTop;
 		
-		return {
-			x: mX - findPosX(t),
-			y: mY - findPosY(t)
-		};
+		return {x: mX - findPosX(t),
+                y: mY - findPosY(t)};
 	}	
 };
