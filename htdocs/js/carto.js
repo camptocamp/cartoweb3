df3d4d8776c046b908baa84d9ffa12d4d643204b
fredj <fredj@4943411f-c6a7-0410-800e-c336a95043a2>
2005-12-23 11:02:28 +0000

    coding style

--- htdocs/js/carto.js
+++ htdocs/js/carto.js
@@ -2,25 +2,27 @@
    Licensed under the GPL (www.gnu.org/copyleft/gpl.html) */
 
 function FormItemSelected() {
-  doSubmit();
+    doSubmit();
 }
 
 function setActiveToolButton(toolid) {
-  for (var i = 0; i < cw3_tools.length; i++) {
-    var elt = xGetElementById(cw3_tools[i] + "_icon");
+    for (var i = 0; i < cw3_tools.length; i++) {
+        var elt = xGetElementById(cw3_tools[i] + "_icon");
 
-    if(elt == null) return;
+        if(elt == null) {
+            return;
+        }
     
-    if (cw3_tools[i] == toolid) {
-      elt.className = "toolbar_on";
-    } else {
-      elt.className = "toolbar_off";
+        if (cw3_tools[i] == toolid) {
+            elt.className = "toolbar_on";
+        } else {
+            elt.className = "toolbar_off";
+        }
     }
-  }
-  var elt = xGetElementById("tool");
-  elt.value = toolid;
+    var elt = xGetElementById("tool");
+    elt.value = toolid;
 }
 
 function checkRadio(id) {
-  xGetElementById(id).checked = 'checked';
+    xGetElementById(id).checked = 'checked';
 }
