27e0c6939f769d3aafdf6c9456178eef256b5e94
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2005-12-28 14:16:34 +0000

    add

---
+++ scripts/checkdos.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+for i in $(find .. -type f ! -path "../locale/*" ! -path "../include/*" ! -path "../www-data/*" ! -name "*png" ! -name "*gif" ! -name "*jpg" \
+    ! -name "*ttf" ! -name "*dbf" ! -name "*shp" ! -name "*jar" ! -name "*class" \
+    ! -name "*gz" ! -name "*bat" ! -name "*shx" ! -name "*tif" ); do od -c  $i |grep -q '\\r' && echo $i; done
