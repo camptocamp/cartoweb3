c51522b1c221e5d20ec294c64611c202ef5155dc
damien <damien@4943411f-c6a7-0410-800e-c336a95043a2>
2006-01-18 13:42:01 +0000

    - Introduces a javascript Logger (Logger.js) to do some javascript debug

---
+++ htdocs/js/Logger.js
@@ -0,0 +1,53 @@
+/* Copyright 2005 Camptocamp SA. 
+   Licensed under the GPL (www.gnu.org/copyleft/gpl.html) */
+
+
+Logger = {	
+	/* Avaiable log levels:
+	 * 0 = none
+	 * 1 = headers
+	 * 2 = errors
+	 * 3 = warns
+	 * 4 = traces
+	 * 5 = notes
+	 * 6 = confirms
+	 */
+	level: 5,
+	
+	send: function(msg) {
+		if (this.level > 0 && typeof(jsTrace) != 'undefined' ) {
+            jsTrace.send(msg);
+        }
+	},
+	
+	header: function(msg) {
+		if (this.level >= 1) {
+            this.send('<br /><font size="medium"><strong>' + msg + '</strong></font>');
+        }
+	},
+	error: function(msg) {
+		if (this.level >= 2) {
+            this.send('<font color="red">' + 'Error: ' + msg + '</font>');
+        }
+	},	
+	warn: function(msg) {
+		if (this.level >= 3) {
+            this.send('<font color="orange">' + 'Warning: ' + msg + '</font>');
+        }
+	},
+	trace: function(msg) {
+		if (this.level >= 4) {
+            this.send('<font color="lightgray">' + msg + '</font>');
+        }
+	},
+	note: function(msg) {
+		if (this.level >= 5) {
+            this.send('<font color="gray">' + msg + '</font>');
+        }
+	},
+	confirm: function(msg) {
+        if (this.level >= 6) {
+            this.send('<font color="lightgreen">' + 'OK: ' + msg + '</font>');
+        }
+    }
+}
