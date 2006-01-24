d77e284ae742b11e666cb4f8faa76118df1cac1e
damien <damien@4943411f-c6a7-0410-800e-c336a95043a2>
2006-01-24 11:23:56 +0000

    dbl_click_delay = 333; //ms

--- htdocs/js/dhtmlInit.js
+++ htdocs/js/dhtmlInit.js
@@ -5,7 +5,7 @@
  * Double clic parameters
  * (DblClick emulation is required for some browsers)
  */
-dbl_click_delay = 200; // ms
+dbl_click_delay = 333; // ms
 dbl_click_tol = 3; // pixels
 /**
  * Min distance betwwen vertex when drawing lines
