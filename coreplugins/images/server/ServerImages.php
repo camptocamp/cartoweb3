a153fc4040527e825078fd6b16e42f437d0d67fa
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2006-01-25 19:54:43 +0000

    temporary rollback with projection check

--- coreplugins/images/server/ServerImages.php
+++ coreplugins/images/server/ServerImages.php
@@ -187,8 +187,9 @@ class ServerImages extends ClientResponderAdapter
                 $msMapObj->setRotation($requ->mainmap->angle);
                 if ($requ->mainmap->angle) {
                     $projection = $msMapObj->getProjection();
-                    if (empty($projection)) {
-                        throw new CartoserverException('no projection defined');
+                    if ($projection == MS_FALSE) {
+                        // FIXME!!
+                        //throw new CartoserverException('no projection defined');
                     }
                     for ($i = 0; $i < $msMapObj->numlayers; $i++) {
                         $layer = $msMapObj->getLayer($i);
