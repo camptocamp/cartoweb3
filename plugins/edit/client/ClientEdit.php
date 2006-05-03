ac257b08fc68c8bad07e9b0f4e46c430d85d56fe
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2006-05-03 08:37:31 +0000

    typo

--- plugins/edit/client/ClientEdit.php
+++ plugins/edit/client/ClientEdit.php
@@ -315,12 +315,12 @@ class ClientEdit extends ClientPlugin
             $this->getConfig()->editDisplayAction : 'both';       
 
         $template->assign(array('edit_active' => true,
-                               'edit_allowed' => $allowed,
-                               'edit_snapping' => $this->editState->snapping,
-                               'edit_shape_type' => $this->editState->shapeType,
-                               'edit_max_insert' => $edit_max_insert,
-                               'edit_resultNbCol' => $editResultNbCol,
-                               'edit_displayAction' => $editDisplayAction));
+                                'edit_allowed' => $allowed,
+                                'edit_snapping' => $this->editState->snapping,
+                                'edit_shape_type' => $this->editState->shapeType,
+                                'edit_max_insert' => $edit_max_insert,
+                                'edit_resultNbCol' => $editResultNbCol,
+                                'edit_displayAction' => $editDisplayAction));
                                
         // editable layers list
         $layersList =  $this->getConfig()->editLayers;
