be87742fa1bc99a7f1bcc692e6b753f49b31a04c
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2006-07-26 13:10:35 +0000

    initial import

---
+++ plugins/exportDxf/client/ClientExportDxf.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ * Exportation of outline shapes in DXF format.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @copyright 2006 Camptocamp SA
+ * @package Plugins
+ * @author Alexandre Saunier
+ * @version $Id$
+ */
+
+require_once CARTOWEB_HOME . 'client/ExportPlugin.php';
+
+/**
+ * @package Plugins
+ */
+class DxfShape {
+    
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $label;
+
+    /**
+     * @var array of Point
+     */
+    public $points;
+
+    /**
+     * Constructor
+     */
+    public function __construct($type, $points, $label = '') {
+        $this->type   = $type;
+        $this->points = $points;
+        $this->label  = $label;
+    }
+}
+
+/**
+ * @package Plugins
+ */
+class ClientExportDxf extends ExportPlugin {
+
+    /**
+     * @var Logger
+     */
+    private $log;
+
+    /**
+     * @var ClientOutline
+     */
+    protected $outline;
+
+    /**
+     * @var integer
+     */
+    protected $roundLevel;
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        $this->log =& LoggerManager::getLogger(__CLASS__);
+        parent::__construct();
+    }
+
+    /**
+     * @see PluginBase::initialize()
+     */
+    public function initialize() {
+        $this->outline = $this->getCartoclient()->getPluginManager()
+                              ->getPlugin('outline');
+        if (is_null($this->outline)) {
+            throw new CartoclientException(
+                'exportDxf requires plugin outline to be loaded');
+        }
+    }
+
+    /**
+     * @see GuiProvider::handleHttpPostRequest()
+     */
+    public function handleHttpPostRequest($request) {}
+
+    /**
+     * @see GuiProvider::handleHttpGetRequest()
+     */
+    public function handleHttpGetRequest($request) {}
+
+    /**
+     * @see GuiProvider::renderForm()
+     */
+    public function renderForm(Smarty $template) {
+        // Export button is displayed only if there are shapes to export.
+        if ($this->outline->hasShapes()) {
+            $smarty = new Smarty_Plugin($this->getCartoclient(), $this);
+            $smarty->assign(array('exportDxfUrl' => $this->getExportUrl()));
+            $exportDxf = $smarty->fetch('form.tpl');
+        } else {
+            $exportDxf = '';
+        }
+        $template->assign('exportDxf', $exportDxf);
+    }
+
+    /**
+     * Returns the number of digits displayed after the comma 
+     * in points coordinates.
+     * @return integer
+     */
+    protected function getRoundLevel() {
+        if (!isset($this->roundLevel)) {
+            $this->roundLevel = (int)$this->getConfig()->roundLevel;
+        }
+        return $this->roundLevel;
+    }
+
+    /**
+     * Returns a Point object with rounded coordinates.
+     * @return Point
+     */
+    protected function getDxfPoint(Point $point) {
+        $roundLevel =& $this->getRoundLevel();
+        $x = round($point->x, $roundLevel);
+        $y = round($point->y, $roundLevel);
+        return new Point($x, $y);
+    }
+
+    /**
+     * Replaces accentuated letters by non accentuated ones.
+     * @param string
+     * @return string
+     */
+    protected function removeAccents($str) {
+        // FIXME: encoding problem?
+        $from = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ';
+        $to   = 'AAAAAAaaaaaaOOOOOOooooooEEEEeeeeCcIIIIiiiiUUUUuuuuyNn';
+        $str  = Encoder::decode($str);
+        $str  = strtr($str, $from, $to);
+        return $str;
+    } 
+
+    /**
+     * @see ExportPlugin::getExportResult
+     */
+    protected function getExport() {
+        $shapes = array();
+        $mapRequest = $this->getLastMapRequest();
+
+        if (empty($mapRequest->outlineRequest) || 
+            empty($mapRequest->outlineRequest->shapes)) {
+            return new ExportOutput();
+        }
+        
+        foreach ($mapRequest->outlineRequest->shapes as $shape) {
+            $type = get_class($shape->shape);
+            if (!in_array($type, array('Point', 'Line', 'Polygon'))) {
+                throw new CartoclientException(
+                    "exportDxf does not handle shape type '$type'");
+            }
+            $label = $this->removeAccents($shape->label);
+            $points = array();
+            if ($type == 'Point') {
+                $points[] = $this->getDxfPoint($shape->shape);
+            } else {
+                foreach ($shape->shape->points as $point) {
+                    $points[] = $this->getDxfPoint($point);
+                }
+            }     
+            $shapes[] = new DxfShape($type, $points, $label);
+        }
+
+        $output = new ExportOutput();
+        $smarty = new Smarty_Plugin($this->getCartoclient(), $this);
+        $smarty->assign('shapes', $shapes);
+        $output->setContents($smarty->fetch('export.dxf.tpl'));
+        return $output;
+    }
+
+    /**
+     * @see ExportPlugin::output()
+     */
+    public function output() {
+        $dxfBuffer = $this->getExport()->getContents();
+        header('Content-Type: application/dxf');
+        header('Content-Length: ' . strlen($dxfBuffer));
+        header('Content-Disposition: attachment; filename=' .
+               $this->getFilename());
+        print $dxfBuffer;
+        return '';
+    }
+
+    /**
+     * Builds exported file name.
+     * @return string
+     */
+     protected function getFilename() {
+         $filename = $this->getConfig()->fileName;
+         if ($filename && preg_match('/^(.*)\[date,(.*)\](.*)$/',
+                                     $filename, $regs)) {
+             $filename = $regs[1] . date($regs[2]) . $regs[3];
+         } else {
+             $filename = 'cartoweb_outline.dxf';
+         }
+         return $filename;
+     }
+}
+?>
---
+++ plugins/exportDxf/templates/form.tpl
@@ -0,0 +1,1 @@
+<input type="button" class="form_button" value="{t}Export DXF{/t}" onclick="javascript:redirectTo('{$exportDxfUrl}')" />
