d388945594e0cf4e3b32f7116a4bb16ba6e79469
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2006-08-11 08:43:12 +0000

    added qix

--- scripts/checkdos.sh
+++ scripts/checkdos.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 for i in $(find .. -type f ! -path "../locale/*" ! -path "../include/*" ! -path "../www-data/*" ! -name "*png" ! -name "*gif" ! -name "*jpg" \
-    ! -name "*ttf" ! -name "*dbf" ! -name "*shp" ! -name "*jar" ! -name "*class" \
+    ! -name "*ttf" ! -name "*dbf" ! -name "*shp" ! -name "*jar" ! -name "*class" ! -name "*qix" \
     ! -name "*gz" ! -name "*bat" ! -name "*shx" ! -name "*tif" ! -name "*ico" ); do od -c  $i |grep -q '\\r' && echo $i; done
