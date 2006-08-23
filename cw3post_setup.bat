81fd0c18e200e20f0c3705a4deb4a214d1dcc1fb
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2006-08-23 15:19:26 +0000

    renamed

--- cw3post_setup.bat
+++
@@ -1,4 +0,0 @@
-%1\php\php -n cw3setup.php --install --base-url http://127.0.0.1/cartoweb3/htdocs/ --profile production
-cd %1\scripts
-php refresh_php_ext.php
-del %1\www\cartoweb3\cw3post_setup.bat
