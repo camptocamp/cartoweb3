9c07fca78455e17b900dd10ae76e2eb38a3b6b62
ochriste <ochriste@4943411f-c6a7-0410-800e-c336a95043a2>
2006-09-29 06:02:03 +0000

    initial import

---
+++ scripts/include_addons/include/smarty/plugins/modifier.print_r.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * $RCSfile$
+ *
+ * modified from the modifier included in Gallery
+ * a web based photo album viewer and editor
+ * Copyright (C) 2000-2006 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * Type:     modifier
+ * Name:     print_r
+ * Purpose:  Dump out the object, reasonably formatted
+ * -------------------------------------------------------------
+ */
+function smarty_modifier_print_r($object) {
+
+    $content = "<pre>";
+    $content .= print_r($object, true);
+    $content .= "</pre>";
+
+    return $content;
+}
+?>
