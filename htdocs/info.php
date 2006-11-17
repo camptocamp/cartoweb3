0122a9149466205cc115c2b03e131d862a5063c0
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2006-11-17 17:01:29 +0000

    removed CGI test

--- htdocs/info.php
+++ htdocs/info.php
@@ -4,21 +4,11 @@
  * @version $Id$
  */
 
-// Safety check for Mapserver bug 1322:
-//   http://mapserver.gis.umn.edu/bugs/show_bug.cgi?id=1322
-// WARNING: this code should be in sync between:
-//  server/ServerContext.php, htdocs/info.php and scripts/info.php
-if (!extension_loaded('mapscript')) {
-    if (!dl('php_mapscript.' . PHP_SHLIB_SUFFIX))
-        print("WARNING: can't load mapscript library");
-} else if (!in_array(substr(php_sapi_name(), 0, 3), array('cgi', 'cli'))) {
-    print("WARNING: You are not using PHP as " .
-        "a cgi and PHP Mapscript extension is loaded in your " .
-        "php.ini.\n This will cause stability problems.\n" .
-        "You need to remove the " .
-        "php_mapscript extension loading of your php.ini " .
-        "file.");
+if (!extension_loaded('mapscript') && 
+    !dl('php_mapscript.' . PHP_SHLIB_SUFFIX)) {
+    print("WARNING: can't load mapscript library");
 }
+
 phpinfo();
 
 ?>
