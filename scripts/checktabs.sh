5c0215343bbee7ce6b5cd49a0788564bc77d23e1
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2006-12-20 08:40:57 +0000

    added js in tab checks

--- scripts/checktabs.sh
+++ scripts/checktabs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 find .. -type f -a \( -name '*dist' -o  -name '*php' -o -name '*tpl' -o -name '*wsdl' \
      -o -name '*xml' -o -name "*map" -o -name "*sym" -o -name "*.c" -o -name "*cpp" \
-     -o -name "*README*" -o -name "*.sql.in" \) \
-|xargs grep -l '	'|grep -v include/pear|grep -v '/include/'|grep -v 'vera/README.TXT'
+     -o -name "*README*" -o -name "*.sql.in" -o -name "*js" \) \
+|xargs grep -l '	'|grep -v include/pear|grep -v '/include/'|grep -v 'vera/README.TXT'|grep -v htdocs/js/wz_jsgraphics.js
+
