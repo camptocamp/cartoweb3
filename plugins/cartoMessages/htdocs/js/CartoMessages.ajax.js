0757726f9e679866dae4cb103cad2e8fffbd2620
ochriste <ochriste@4943411f-c6a7-0410-800e-c336a95043a2>
2007-05-03 07:50:38 +0000

    decoupled user and dev message, dev message are formated and printed in the main page

--- plugins/cartoMessages/htdocs/js/CartoMessages.ajax.js
+++ plugins/cartoMessages/htdocs/js/CartoMessages.ajax.js
@@ -9,25 +9,36 @@ AjaxPlugins.CartoMessages = {
     notifyDeveloperMsgs: false,
     
     handleResponse: function(pluginOutput) {
+
+        // check profile
+        if (AjaxHandler.getProfile() == 'development') {
+            this.notifyDeveloperMsgs = true;
+        }
+
         // Shows developer and user messages in jsTrace debugger window
         if (pluginOutput.htmlCode.developerMessages != '') {
             var developerMsgs = eval(pluginOutput.variables.developerMessages);
             Logger.trace('Developer messages: ' + developerMsgs.length);
             if (developerMsgs.length > 0) Logger.note(this.formatHtml(developerMsgs));
-            if (this.notifyDeveloperMsgs) this.notify(developerMsgs);
+            if (this.notifyDeveloperMsgs) this.notifyDeveloper(developerMsgs);
         }
         if (pluginOutput.htmlCode.userMessages != '') {
             var userMsgs = eval(pluginOutput.variables.userMessages);
             Logger.trace('User messages: ' + userMsgs.length);
             if (userMsgs.length > 0) Logger.note(this.formatHtml(userMsgs));
-            if (this.notifyUserMsgs) this.notify(userMsgs);
+            if (this.notifyUserMsgs) this.notifyUser(userMsgs);
         }
     },
     
-    notify: function(messageArray, header) {
+    notifyUser: function(messageArray, header) {
         if (messageArray.length < 1) return;
         var header = header || 'Message(s): \r\n \r\n';
-        alert(header + this.formatAlertBox(messageArray));
+        alert(header + this.formatAlertBox(messageArray));        
+    },
+
+    notifyDeveloper: function(messageArray, header) {
+        if (messageArray.length < 1) return;
+        this.outputMessages(messageArray);
     },
     
     formatHtml: function(messageArray) {
@@ -43,8 +54,22 @@ AjaxPlugins.CartoMessages = {
     formatAlertBox: function(messageArray) {
         var formattedString = '';
         messageArray.each(function(msg) {
+            msg = msg.replace(/&#039;/g, "'");
             formattedString += '\t * ' + msg + '\r\n';
         });
         return formattedString;
+    },
+
+    outputMessages: function(messageArray) {
+        outputStringTarget = xGetElementById('developperMsgs');
+        if (outputStringTarget) {
+            var outputString = "Developer Messages: <br />";
+            messageArray.each(function(msg) {
+                msg = msg.replace(/\,/g, ",\n");
+                outputString += '<pre>' + msg + '\n##(notice: line return added '
+                             + 'after , for better display)##</pre>';
+            });
+            outputStringTarget.innerHTML = outputString;
+        }
     }
 };
