71ef0c41ebbed67c8645e737c62964d71687a5ba
ochriste <ochriste@4943411f-c6a7-0410-800e-c336a95043a2>
2007-07-04 06:32:43 +0000

    changed script to work with ms4w

--- post_setup_main.bat
+++ post_setup_main.bat
@@ -1,4 +1,10 @@
-%1\php\php -n cw3setup.php --install --base-url http://localhost/cartoweb3/htdocs/ --profile production
-cd %1\scripts
-%1\php\php -n refresh_php_ext.php
-del %1\www\cartoweb3\post_setup_main.bat
+@ECHO OFF
+
+ECHO setup CartoWeb
+REM setup CartoWeb
+%1\Apache\cgi-bin\php -n cw3setup.php --install --base-url http://localhost/cartoweb3/htdocs/ --profile production
+REM restart apache
+cd %1
+call apache-restart.bat
+REM delete post setup script
+del %1\apps\cartoweb3\post_setup_main.bat
