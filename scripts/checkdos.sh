fa998b51b6bc1180f15eeb8b44289ccf8935332d
sypasche <sypasche@4943411f-c6a7-0410-800e-c336a95043a2>
2008-03-13 10:41:35 +0000

    Removed artichow check.

--- scripts/checkdos.sh
+++ scripts/checkdos.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# Albert, Abel, Example-006 are artichow files. Remove the rules once moved to cartoweb_includes
+
 for i in $(find .. -type f ! -path "../locale/*" ! -path "../include/*" ! -path "../www-data/*" ! -iname "*png" ! -iname "*gif" ! -iname "*jpg" \
     ! -iname "*ttf" ! -iname "*dbf" ! -iname "*shp" ! -iname "*jar" ! -iname "*class" ! -iname "*qix" \
-    ! -iname "*gz" ! -iname "*bat" ! -iname "*shx" ! -iname "*rtf" ! -iname "*tif" ! -iname "*tiff" ! -iname "*ico" ! -iname "*db"); do od -c  $i |grep -q '\\r' && echo $i; done
+    ! -iname "*gz" ! -iname "*bat" ! -iname "*shx" ! -iname "*rtf" ! -iname "*tif" ! -iname "*tiff" ! -iname "*ico" ! -iname "*db" \
+    ! -name Albert ! -name Abel ! -name Example-006 ); \
+    do od -c  $i |grep -q '\\r' && echo $i; done
