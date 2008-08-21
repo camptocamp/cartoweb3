2bb049fe609fffef8583001130635b2fb5eac95a
ochriste <ochriste@4943411f-c6a7-0410-800e-c336a95043a2>
2008-08-21 08:23:15 +0000

    removed tabs

--- htdocs/js/prototype.js
+++ htdocs/js/prototype.js
@@ -2741,7 +2741,7 @@ var Selector = Class.create({
         p = ps[i];
         if (m = e.match(p)) {
           this.matcher.push(Object.isFunction(c[i]) ? c[i](m) :
-    	      new Template(c[i]).evaluate(m));
+            new Template(c[i]).evaluate(m));
           e = e.replace(m[0], '');
           break;
         }
@@ -3074,7 +3074,7 @@ Object.extend(Selector, {
 
     nextElementSibling: function(node) {
       while (node = node.nextSibling)
-	      if (node.nodeType == 1) return node;
+        if (node.nodeType == 1) return node;
       return null;
     },
 
