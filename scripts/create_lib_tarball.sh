6dc43b1e1cb7ee0c40a762111cd6df61e6fc6066
asaunier <asaunier@4943411f-c6a7-0410-800e-c336a95043a2>
2008-08-28 15:29:08 +0000

    updated Smarty URL

--- scripts/create_lib_tarball.sh
+++ scripts/create_lib_tarball.sh
@@ -33,7 +33,7 @@ PEAR_PACKAGES_DEVEL="PhpDocumentor"
 
 
 LOG4PHP="http://www.vxr.it/log4php/log4php-0.9.tar.gz"
-SMARTY="http://smarty.php.net/do_download.php?download_file=Smarty-2.6.19.tar.gz"
+SMARTY="http://smarty.net/do_download.php?download_file=Smarty-2.6.19.tar.gz"
 FPDF="http://www.fpdf.org/fr/dl.php?v=153&f=tgz"
 THEMAMAP="http://cartoweb.org/downloads/themamap/themamap-0.41.tar.gz" 
 ARTICHOW="http://www.artichow.org/data/Artichow-1.1.0-php5.tar.gz"
